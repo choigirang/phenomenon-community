6458c8f16f96aa2c54705c68ddd718cee330a0a0
GiRang Choi <choigirang@kakao.com>
2023-06-06 18:42:35 +0900

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,11 @@ import styled from 'styled-components';
 export default function Header() {
   const category = ['명예의 전당', '게시글', '공지사항'];
 
-  return <Container>Header</Container>;
+  return (
+    <Container>
+      <div className="logo">logo</div>
+    </Container>
+  );
 }
 
 const Container = styled.div`
@@ -12,4 +16,14 @@ const Container = styled.div`
   height: 60px;
   position: relative;
   background: gray;
+
+  .logo {
+    position: absolute;
+    left: 20px;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 200px;
+    height: 30px;
+    background: white;
+  }
 `;
