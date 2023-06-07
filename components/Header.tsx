3e01c0792ea8ed0704898536421e015f554c02a3
GiRang Choi <choigirang@kakao.com>
2023-06-07 23:06:05 +0900

    feat: Header 라우터

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,21 @@
+import { useRouter } from 'next/router';
 import React from 'react';
 import styled from 'styled-components';
 
 export default function Header() {
   const category = ['명예의 전당', '게시글', '공지사항'];
+  const router = useRouter();
 
   return (
     <Container>
       <div className="logo">logo</div>
+      <div className="category-box">
+        {category.map(each => (
+          <a className="category-text" onClick={() => router.push(each)} key={each}>
+            {each}
+          </a>
+        ))}
+      </div>
     </Container>
   );
 }
@@ -25,5 +34,20 @@ const Container = styled.div`
     width: 200px;
     height: 30px;
     background: white;
+  }
+
+  .category-box {
+    display: flex;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    color: white;
+  }
+
+  .category-text {
+    padding: 5px;
+    font-size: var(--size-title);
+    padding: var(--padding-text);
   }
 `;
