89e31c65323f9b4584e87b8bb223f9ebbb984130
GiRang Choi <choigirang@kakao.com>
2023-06-08 18:19:19 +0900

    Update LeftBox.tsx

--- components/Home/LeftBox.tsx
+++ components/Home/LeftBox.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export default function LeftBox() {
   return (
-    <>
+    <div className="first-box">
       <ul className="hof-article"></ul>
-    </>
+    </div>
   );
 }
+
+// const Container = styled.div`
+//   width: 70%;
+// `;
