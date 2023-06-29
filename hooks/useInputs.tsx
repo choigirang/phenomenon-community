73bb793e269877fe8c5dc853674718414ad88060
GiRang Choi <choigirang@kakao.com>
2023-06-29 17:11:36 +0900

    feat: input hooks 만들기

--- hooks/useInputs.tsx
+++ hooks/useInputs.tsx
@@ -1,7 +1,12 @@
 import { useState } from 'react';
 
+// 초깃값 없을 시 2중 에러
 function useInputs(initialData: string) {
-  const [data, setData] = useState();
+  const [data, setData] = useState(initialData);
 
-  const onChange = () => {};
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setData(e.target.value);
+  };
+
+  return [data, onChange, setData];
 }
