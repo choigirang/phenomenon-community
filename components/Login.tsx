9e0cc12de860ff0f2d4f88536cfa7eda890fd04e
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-07-03 18:52:32 +0900

    feat: Login components

---
+++ components/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Login() {
+  return <Container>Login</Container>;
+}
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: gray;
+`;
