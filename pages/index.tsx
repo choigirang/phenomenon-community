a79178145b73d6da8a0fb3e1abf004f33551b565
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-07-03 18:53:05 +0900

    feat: grid 추가 필요

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,25 @@ import Head from 'next/head';
 import styled from 'styled-components';
 import LeftBox from '../components/Home/LeftBox';
 import axios from 'axios';
+import Login from '@/components/Login';
 
 export default function Home() {
   return (
-    <Container>
+    <>
       <Head>
         <title>이왜진</title>
       </Head>
-      <LeftBox />
-    </Container>
+      <Container>
+        <LeftBox />
+        <Login />
+      </Container>
+    </>
   );
 }
 
 const Container = styled.div`
-  width: 100%;
   height: 100%;
+  padding: 0 calc(100% - 1280px);
+  display: grid;
+  grid-template-rows: 70% 30%;
 `;
