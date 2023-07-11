24606655c54070f77512fa14c3aef0addee1e736
GiRang Choi <choigirang@kakao.com>
2023-07-11 14:40:41 +0900

    feat: 상단 카테고리

--- components/Home/LeftBox.tsx
+++ components/Home/LeftBox.tsx
@@ -2,11 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 
 export default function LeftBox() {
+  const category = ['임시'];
+
+  const tem = Array(10).fill(category).flat();
+
   return (
     <Container>
-      <div className="first-box">
-        <ul className="hof-article"></ul>
-      </div>
+      <CategoryBox>
+        <Category>
+          <p className="sub-title">전체보기</p>
+          {tem.map(item => (
+            <div>{item}</div>
+          ))}
+        </Category>
+      </CategoryBox>
     </Container>
   );
 }
@@ -14,5 +23,25 @@ export default function LeftBox() {
 const Container = styled.div`
   width: 100%;
   height: 100%;
-  background: lightblue;
+`;
+
+const CategoryBox = styled.div`
+  width: 100%;
+  height: 170px;
+  padding: var(--padding-solo) 0;
+`;
+
+const Category = styled.div`
+  width: 100%;
+  height: 100%;
+  border: var(--border-content);
+  padding: var(--padding-content);
+
+  .sub-title {
+    width: 100%;
+    font-weight: 500;
+    font-size: 12px;
+    padding-bottom: var(--padding-solo);
+    border-bottom: var(--border-dash);
+  }
 `;
