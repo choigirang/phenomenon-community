1990dd39d888261521322628a0b28a5daca49118
GiRang Choi <choigirang@kakao.com>
2023-07-12 14:18:37 +0900

    feat: 카테고리,포스팅 리스트 작성

--- components/Home/LeftBox.tsx
+++ components/Home/LeftBox.tsx
@@ -1,21 +1,48 @@
-import React from 'react';
+import { PostType } from '@/types/type';
+import axios from 'axios';
+import Image from 'next/image';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 export default function LeftBox() {
+  const [post, setPost] = useState<PostType[]>();
   const category = ['임시'];
 
   const tem = Array(10).fill(category).flat();
+
+  useEffect(() => {
+    axios.get('/dummy').then(res => {
+      setPost(res.data);
+    });
+  }, []);
 
   return (
     <Container>
       <CategoryBox>
         <Category>
           <p className="sub-title">전체보기</p>
-          {tem.map(item => (
-            <div>{item}</div>
-          ))}
+          <CategoryList>
+            {tem.map((item, idx) => (
+              <CategoryItem key={idx}>
+                <span>{item}</span>
+              </CategoryItem>
+            ))}
+          </CategoryList>
         </Category>
       </CategoryBox>
+      <PostBox>
+        <p className="sub-title">실시간 게시글</p>
+        <PostList>
+          {post &&
+            post.map(item => (
+              <Post key={item.id}>
+                <a className="">
+                  <span className="post-title">{item.title}</span>
+                </a>
+              </Post>
+            ))}
+        </PostList>
+      </PostBox>
     </Container>
   );
 }
@@ -25,6 +52,7 @@ const Container = styled.div`
   height: 100%;
 `;
 
+// 카테고리 상자
 const CategoryBox = styled.div`
   width: 100%;
   height: 170px;
@@ -44,4 +72,50 @@ const Category = styled.div`
     padding-bottom: var(--padding-solo);
     border-bottom: var(--border-dash);
   }
+`;
+
+// 카테고리 목록
+const CategoryList = styled.ul`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const CategoryItem = styled.li`
+  padding: var(--padding-content);
+  padding-left: 0;
+  font-size: var(--size-text);
+
+  span:hover {
+    cursor: pointer;
+    border-bottom: var(--border-text);
+    font-weight: 400;
+  }
+`;
+
+// 게시글 목록
+const PostBox = styled.div`
+  width: 100%;
+
+  .sub-title {
+    width: 100%;
+    color: var(--color-black);
+    font-weight: 400;
+    font-size: 15px;
+    margin-top: var(--padding-side);
+    padding-bottom: var(--padding-solo);
+    border-bottom: solid 3px var(--color-blue);
+  }
+`;
+
+const PostList = styled.ul`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Post = styled.li`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
 `;
