39c28276710e3fec144a5710f409966b9b65464a
GiRang Choi <choigirang@kakao.com>
2023-07-12 14:19:05 +0900

    feat: 데이터 형식 및 수정

--- mocks/handlers.tsx
+++ mocks/handlers.tsx
@@ -1,10 +1,15 @@
 import { rest } from 'msw';
 
 const post = [
-  { userId: 1, name: 'choi', content: 'content' },
-  { userId: 2, name: 'choi', content: 'content' },
-  { userId: 3, name: 'choi', content: 'content' },
-  { userId: 4, name: 'choi', content: 'content' },
+  { id: 9, userId: 1, name: 'choi', title: '9번 게시글 입니다.', content: 'content', createdAt: '2023-07-12' },
+  { id: 8, userId: 1, name: 'choi', title: '8번 게시글 입니다.', content: 'content', createdAt: '2023-07-12' },
+  { id: 7, userId: 1, name: 'choi', title: '7번 게시글 입니다.', content: 'content', createdAt: '2023-07-12' },
+  { id: 6, userId: 1, name: 'choi', title: '6번 게시글 입니다.', content: 'content', createdAt: '2023-07-12' },
+  { id: 5, userId: 1, name: 'choi', title: '5번 게시글 입니다.', content: 'content', createdAt: '2023-07-12' },
+  { id: 4, userId: 1, name: 'choi', title: '4번 게시글 입니다.', content: 'content', createdAt: '2023-07-12' },
+  { id: 3, userId: 1, name: 'choi', title: '3번 게시글 입니다.', content: 'content', createdAt: '2023-07-12' },
+  { id: 2, userId: 1, name: 'choi', title: '2번 게시글 입니다.', content: 'content', createdAt: '2023-07-12' },
+  { id: 1, userId: 1, name: 'choi', title: '1번 게시글 입니다.', content: 'content', createdAt: '2023-07-12' },
 ];
 
 export const handler = [
