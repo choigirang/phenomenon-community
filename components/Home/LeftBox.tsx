02393f17a6b933659405db3ff207f68130864c17
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-07-13 20:49:28 +0900

    feat: query 페이지네이션

--- components/Home/LeftBox.tsx
+++ components/Home/LeftBox.tsx
@@ -1,7 +1,7 @@
 import { PostType } from '@/types/type';
 import axios from 'axios';
 import Image from 'next/image';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { AiFillLeftSquare, AiFillRightSquare } from 'react-icons/ai';
 import { useQuery } from 'react-query';
 import styled from 'styled-components';
@@ -9,17 +9,32 @@ import styled from 'styled-components';
 const POST_PAGE = 10;
 
 export default function LeftBox() {
+  // 게시글 페이지네이션
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
   const [post, setPost] = useState<PostType[]>();
   const [curIndex, setCurIndex] = useState<number>(0);
+  // 카테고리 가져오기
   const category = ['임시'];
-
-  const fetch = () => {
-    return axios.get('/dummy');
-  };
-
-  const { isLoading, data, isError, error } = useQuery('post', fetch);
-
   const tem = Array(30).fill(category).flat();
+
+  // 임시 데이터
+  async function fetch() {
+    return await axios.get('/dummy').then(res => res.data);
+  }
+
+  const { isLoading, data, isError, error } = useQuery<PostType[]>(['post'], () => fetch(), { staleTime: 2000 });
+
+  // 데이터 함수
+  // 게시글 데이터 함수
+  // async function fetch(page: number) {
+  //   return await axios.get(`https://locallhost:3000/post?page?=${currentPage}`).then(res => res.data);
+  // }
+
+  // 게시글 데이터 쿼리
+  // const { isLoading, data, isError, error } = useQuery<PostType[]>(['post'], () => fetch(currentPage), {
+  //   staleTime: 2000,
+  // });
 
   return (
     <Container>
@@ -38,13 +53,20 @@ export default function LeftBox() {
       <PostTitle>
         <p className="sub-title">실시간 게시글</p>
         <div className="btn-box">
-          <AiFillLeftSquare />
-          <AiFillRightSquare />
+          <div className="text-box">
+            <span className="cur-page">{currentPage}</span>/<span className="total-page">{POST_PAGE}</span>
+          </div>
+          <button disabled={currentPage <= 1} onClick={() => setCurrentPage(pre => pre - 1)}>
+            <AiFillLeftSquare />
+          </button>
+          <button disabled={currentPage >= POST_PAGE} onClick={() => setCurrentPage(pre => pre + 1)}>
+            <AiFillRightSquare />
+          </button>
         </div>
       </PostTitle>
       <PostList>
         {data &&
-          data.data.map(item => (
+          data.map(item => (
             <Post key={item.id}>
               <a href="/" className="post-box">
                 <span className="post-title">{item.title}</span>
@@ -119,12 +141,33 @@ const PostTitle = styled.div`
     color: var(--color-black);
     font-weight: 400;
     font-size: 15px;
-    margin-top: var(--padding-side);
-    padding-bottom: var(--padding-solo);
+    margin: var(--padding-side) 0;
   }
 
   .btn-box {
     display: flex;
+    align-items: center;
+
+    .text-box {
+      display: flex;
+      font-size: var(--size-text);
+
+      .cur-page {
+        color: var(--color-red);
+        font-weight: 500;
+      }
+
+      .total-page {
+        display: flex;
+        margin-right: calc(var(--margin-small) - 5px);
+        font-size: var(--size-text);
+      }
+    }
+
+    button {
+      display: flex;
+      align-items: center;
+    }
 
     svg {
       cursor: pointer;
