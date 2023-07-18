9052e4cbd17e477643f7ac9adf63fb1d82cccd0f
GiRang Choi <choigirang@kakao.com>
2023-07-18 17:54:56 +0900

    feat: 폴더 변경

--- frontend/components/Home/RightBox.tsx
+++ frontend/components/Home/RightBox.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Login from './Login';
+import Login from './../Login';
 
 export default function RightBox() {
   return (
