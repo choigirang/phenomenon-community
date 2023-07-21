b4278af17558996b4671e01f3dd8efe6b235cd54
GiRang Choi <choigirang@kakao.com>
2023-07-21 23:45:46 +0900

    feat: 회원가입 페이지

--- frontend/pages/signup/index.tsx
+++ frontend/pages/signup/index.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import { HEADER_NAV } from '@/constant/constant';
+import { useRouter } from 'next/router';
+import React, { useMemo } from 'react';
 import styled from 'styled-components';
 
 export default function index() {
+  // Header 카테고리
+  const category = useMemo(() => Object.keys(HEADER_NAV), []);
+
+  // 링크
+  const router = useRouter();
+
   return (
     <>
-      <Top>index</Top>
+      <Top>
+        <div className="logo" onClick={() => router.push('/')}>
+          logo
+        </div>
+        <Category>
+          {category.map(each => (
+            <li key={HEADER_NAV[each]} className="nav-item">
+              <a onClick={() => router.push(HEADER_NAV[each])}>{each}</a>
+            </li>
+          ))}
+        </Category>
+      </Top>
+      <Bottom></Bottom>
     </>
   );
 }
@@ -14,11 +34,21 @@ const Top = styled.div`
   height: 100px;
   display: flex;
   align-items: center;
+  justify-content: space-between;
   background-color: var(--color-blue);
   color: white;
   padding: 0 calc(100% - 1280px);
 `;
 
+const Category = styled.ul`
+  display: flex;
+  gap: 8px;
+  font-size: 12px;
+`;
+
 const Bottom = styled.div`
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
