f90f30fa2f5b34b1a9339a377738bb9f495a48b3
GiRang Choi <choigirang@kakao.com>
2023-07-21 23:45:24 +0900

    feat: Link 데이터

--- frontend/constant/constant.tsx
+++ frontend/constant/constant.tsx
@@ -1,10 +1,21 @@
 import { HeaderNav } from '@/types/type';
 
+// 카테고리
 export const HEADER_NAV: HeaderNav = {
   전체보기: '/',
   명예의전당: 'hof',
   게시글: 'community',
   공지사항: 'notice',
+};
+
+// 회원가입 링크
+export const SIGN: HeaderNav = {
   회원가입: 'signup',
   회원찾기: 'findUser',
+};
+
+export const SIGN_UP: HeaderNav = {
+  약관동의: '/agree',
+  정보입력: '/info',
+  가입완료: '/',
 };
