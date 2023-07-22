f3b0d2f19746d4c11b519e1ad823147edf345797
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-07-22 15:52:59 +0900

    feat: global-styled-components 분리

---
+++ frontend/styles/GlobalComponents.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+// 로고
+export const Logo = styled.img`
+  width: ${props => props.width || '250px'};
+  height: ${props => props.height || '50px'};
+`;
+
+// 회원가입 완료 버튼
+export const NextPage = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: end;
+
+  .btn {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-width: 100px;
+    min-height: 40px;
+    color: white;
+    font-size: var(--size-sub-title);
+    font-weight: 500;
+    padding: var(--padding-text);
+    background-color: var(--color-gray);
+    border: solid 2px #606060;
+    cursor: pointer;
+  }
+`;
