6ab05d1d7b13cfe149b35c7d9ff327aa66e6c26a
GiRang Choi <choigirang@kakao.com>
2023-08-01 23:11:43 +0900

    delete: mockup data

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
-import { postData } from "./data/postData";
-import { user } from "./data/loginData";
+import mongoose from "mongoose";
 
 const express = require("express");
 const cors = require("cors");
@@ -8,23 +7,12 @@ const app = express();
 const port = 3001;
 
 app.use(cors());
+app.use(express.json());
 app.listen(port);
-// 게시글 관련
-app.get("/", (req: Request, res: Response) => {
-  res.send(postData);
-});
 
-// 로그인 관련
-app.get("/login", (req: Request, res: Response) => {
-  const { id, password } = req.query;
-
-  const findUser = user.find(
-    (user) => user.id === id && user.password === password
-  );
-  if (findUser) {
-    return res.send(findUser);
-  } else {
-    console.log(user);
-    res.status(401).json({ message: "일치하지 않는 사용자입니다." });
-  }
-});
+mongoose
+  .connect(
+    "mongodb+srv://chlrlfkd:chlrlfkd5633@phenomenon-community.zyo8dzo.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("connected successfully"))
+  .catch((err) => console.log(err));
