be8aeaa2d48b14af7d077650ed1798e0e142b319
GiRang Choi <choigirang@kakao.com>
2023-08-02 00:22:22 +0900

    feat: userSchema

---
+++ backend/src/models/users.model.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  userNum: {
+    type: Number,
+    required: true,
+  },
+  id: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  mail: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export { User };
