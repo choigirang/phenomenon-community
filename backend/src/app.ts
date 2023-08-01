ed02f5920aaa8510425fbfffe3c8e5f1d7cc4ed4
GiRang Choi <choigirang@kakao.com>
2023-08-02 00:22:42 +0900

    feat: CommonJS => ES6

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,15 @@
-import { Request, Response } from "express";
-import mongoose from "mongoose";
+import express from "express";
+import mongoose, { Error } from "mongoose";
+import path from "path";
+import cors from "cors";
+import { usersRouter } from "../routes/users.router";
 
-const express = require("express");
-const cors = require("cors");
 const app = express();
 const port = 3001;
 
 app.use(cors());
 app.use(express.json());
+app.use(usersRouter);
 app.listen(port);
 
 mongoose
@@ -15,4 +17,4 @@ mongoose
     "mongodb+srv://chlrlfkd:chlrlfkd5633@phenomenon-community.zyo8dzo.mongodb.net/?retryWrites=true&w=majority"
   )
   .then(() => console.log("connected successfully"))
-  .catch((err) => console.log(err));
+  .catch((err: Error) => console.log(err));
