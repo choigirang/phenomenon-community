41dcd8b3a2c14a801bd01b861825c2d57e9901bb
GiRang Choi <choigirang@kakao.com>
2023-08-03 23:56:51 +0900

    feat: add router

--- backend/routes/users.router.ts
+++ backend/routes/users.router.ts
@@ -1,8 +1,9 @@
-import express from "express";
-import { createUser, showUser } from "../src/controllers/users.controller";
+import express from 'express';
+import { createUser, sendSecurityCode, showUser } from '../src/controllers/users.controller';
 const usersRouter = express.Router();
 
-usersRouter.get("/login", showUser);
-usersRouter.post("/signin", createUser);
+usersRouter.get('/login', showUser);
+usersRouter.post('/signin', createUser);
+usersRouter.post('/signin/security-code', sendSecurityCode);
 
 export { usersRouter };
