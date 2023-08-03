f0150cdde612ab7493236d986b18de9dfe201c29
GiRang Choi <choigirang@kakao.com>
2023-08-03 23:57:35 +0900

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,8 @@
-import express from "express";
-import mongoose, { Error } from "mongoose";
-import path from "path";
-import cors from "cors";
-import { usersRouter } from "../routes/users.router";
+import express from 'express';
+import mongoose, { Error } from 'mongoose';
+import path from 'path';
+import cors from 'cors';
+import { usersRouter } from '../routes/users.router';
 
 const app = express();
 const port = 3001;
@@ -13,8 +13,6 @@ app.use(usersRouter);
 app.listen(port);
 
 mongoose
-  .connect(
-    "mongodb+srv://chlrlfkd:chlrlfkd5633@phenomenon-community.zyo8dzo.mongodb.net/?retryWrites=true&w=majority"
-  )
-  .then(() => console.log("connected successfully"))
+  .connect('mongodb+srv://chlrlfkd:chlrlfkd5633@phenomenon-community.zyo8dzo.mongodb.net/?retryWrites=true&w=majority')
+  .then(() => console.log('connected successfully'))
   .catch((err: Error) => console.log(err));
