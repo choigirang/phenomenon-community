0b3f90631c1799d9a77a9c921272cf9060fe2f92
GiRang Choi <choigirang@kakao.com>
2023-08-03 23:58:10 +0900

    feat: 보안코드 메일 전송
    Error 확인 필요

--- backend/src/controllers/users.controller.ts
+++ backend/src/controllers/users.controller.ts
@@ -1,10 +1,12 @@
-import express, {
-  Request,
-  Response,
-  NextFunction,
-  RequestHandler,
-} from "express";
-import { User } from "../models/users.model";
+import express, { Request, Response, NextFunction, RequestHandler } from 'express';
+import { User } from '../models/users.model';
+import crypto from 'crypto';
+import nodemailer from 'nodemailer';
+
+// 보안 메일
+function generateRandomCode() {
+  return crypto.randomBytes(3).toString('hex').toUpperCase();
+}
 
 const showUser: RequestHandler = async (req, res, next): Promise<void> => {
   try {
@@ -16,6 +18,35 @@ const showUser: RequestHandler = async (req, res, next): Promise<void> => {
   }
 };
 
+async function sendSecurityCode(req: Request, res: Response) {
+  const { email, domain } = req.body;
+  const securityCode = generateRandomCode();
+  try {
+    // 메일 전송 트랜스포터 설정
+    const transporter = nodemailer.createTransport({
+      service: 'Gamil',
+      auth: {
+        user: 'chlrlfkd@gmail.com',
+        pass: 'chlrlfkd',
+      },
+    });
+
+    // 메일 옵션
+    const mailOpt = {
+      from: 'chlrlfkd@gmail.com',
+      to: `${email}@${domain}`,
+      subject: 'phenomeno 보안 코드를 입력해주세요.',
+      text: `보안코드 : ${securityCode}`,
+    };
+
+    // 메일 전송
+    const result = await transporter.sendMail(mailOpt);
+    return res.send(result);
+  } catch (err) {
+    return res.send(`메일 전송에 실패하였습니다. ${err}`);
+  }
+}
+
 // 비동기 요청
 async function createUser(req: Request, res: Response, next: NextFunction) {
   try {
@@ -26,4 +57,4 @@ async function createUser(req: Request, res: Response, next: NextFunction) {
   }
 }
 
-export { showUser, createUser };
+export { showUser, sendSecurityCode, createUser };
