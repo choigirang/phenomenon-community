eb1929eba3f2860c42560516229fa4f6a73d902b
GiRang Choi <choigirang@kakao.com>
2023-08-04 14:09:17 +0900

    feat: add cookie parser

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,15 +2,19 @@ import express from 'express';
 import mongoose, { Error } from 'mongoose';
 import path from 'path';
 import cors from 'cors';
-import { usersRouter } from '../routes/users.router';
+import { usersRouter } from './routes/users.router';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const port = 3001;
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use(usersRouter);
-app.listen(port);
+app.listen(port, () => {
+  console.log('서버 실행');
+});
 
 mongoose
   .connect('mongodb+srv://chlrlfkd:chlrlfkd5633@phenomenon-community.zyo8dzo.mongodb.net/?retryWrites=true&w=majority')
