8fc628ad6dd4643dc14932779e705d8886fcfda1
GiRang Choi <choigirang@kakao.com>
2023-08-08 18:30:18 +0900

    feat: cookie 사용 예정에 의한 인증방식 추가

--- frontend/util/api/index.ts
+++ frontend/util/api/index.ts
@@ -5,7 +5,7 @@ const api = axios.create({
   baseURL: 'http://localhost:3001',
   timeout: 5000,
   headers: { 'Content-Type': 'application/json' },
-  // withCredentials: true,
+  withCredentials: true,
   // 이후 쿠키
   // headers: {
   //   'Content-Type': 'application/json',
