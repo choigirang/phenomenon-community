27d9e620d69eebf4517b56c96bcdc92ad8197851
GiRang Choi <choigirang@kakao.com>
2023-08-10 23:24:05 +0900

    feat: refreshToken 추가에 따른 데이터 타입 추가

--- backend/type/type.ts
+++ backend/type/type.ts
@@ -1,12 +1,16 @@
 import { Document } from 'mongoose';
 
 export interface UserType extends Document {
-  userNum: number;
   id: string;
   password: string;
   name: string;
   mail: string;
-  comparePassword: (password: string) => boolean;
+  refreshToken: string;
+}
+
+export interface AuthData {
+  user: UserType;
+  token: string;
 }
 
 export interface PostType extends Document {
