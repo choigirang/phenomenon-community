d087436e44a00953dfcbc9ed8fdfc8741269eb7e
GiRang Choi <choigirang@kakao.com>
2023-08-10 23:21:58 +0900

    feat: 라우터 refreshToken 추가

--- backend/routes/users.router.ts
+++ backend/routes/users.router.ts
@@ -1,9 +1,21 @@
 import express from 'express';
-import { createUser, sendSecurityCode, loginUser } from '../src/controllers/users.controller';
+import { createUser, sendSecurityCode, loginUser, checkUser } from '../src/controllers/users.controller';
+import { refreshToken } from '../src/controllers/token.controller';
 const usersRouter = express.Router();
 
+// 로그인
 usersRouter.post('/login', loginUser);
+
+// 로그인 유지
+usersRouter.get('/user', checkUser);
+
+// 회원가입
 usersRouter.post('/signin', createUser);
+
+// 보안메일
 usersRouter.post('/signin/security-code', sendSecurityCode);
+
+// 토큰 재발급
+usersRouter.post('/refresh', refreshToken);
 
 export { usersRouter };
