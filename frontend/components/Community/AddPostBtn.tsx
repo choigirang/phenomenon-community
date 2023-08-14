556d08dca586fd5937daa072f1623a7da9b95403
GiRang Choi <choigirang@kakao.com>
2023-08-14 18:07:30 +0900

    feat: import 정리

--- frontend/components/Community/AddPostBtn.tsx
+++ frontend/components/Community/AddPostBtn.tsx
@@ -1,5 +1,6 @@
-import { useRouter } from 'next/router';
 import React from 'react';
+import { useRouter } from 'next/router';
+
 import styled from 'styled-components';
 
 export default function AddPostBtn() {
