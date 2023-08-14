e086fd76168d6a59e62f9140de5c2ad2ac5524ed
GiRang Choi <choigirang@kakao.com>
2023-08-14 18:07:14 +0900

    feat: 폴더 정리

--- frontend/components/Common/RightBox.tsx
+++ frontend/components/Common/RightBox.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import Login from './../Login';
+import Login from './Login';
 import AddPostBtn from '../Community/AddPostBtn';
 
 export default function RightBox() {
