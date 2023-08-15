91c4929d0569eba24af0e727b2fd5fb53e09ca5a
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-08-15 22:01:11 +0900

    feat: 로그인 상태에 따른 게시글 작성 버튼

--- frontend/components/Common/RightBox.tsx
+++ frontend/components/Common/RightBox.tsx
@@ -3,12 +3,17 @@ import styled from 'styled-components';
 
 import Login from './Login';
 import AddPostBtn from '../Community/AddPostBtn';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/redux/store';
 
 export default function RightBox() {
+  const user = useSelector((state: RootState) => state.user.user);
+
+  const loginState = user.login && user.name;
   return (
     <Container>
       <Login />
-      <AddPostBtn />
+      {loginState && <AddPostBtn />}
     </Container>
   );
 }
