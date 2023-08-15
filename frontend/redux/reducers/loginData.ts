24620ec78e95e141b3d53e7a90d1b8d252fae7cf
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-08-15 22:05:13 +0900

    feat: 로그인 user 정보 저장 reducer

--- frontend/redux/reducers/loginData.ts
+++ frontend/redux/reducers/loginData.ts
@@ -1,31 +1,34 @@
-import { UserType } from '@/types/type';
-import { SET_LOGGED_IN_USER, SetLoggedInUserAction } from '../actions/user';
+// reducers.js
+import { UserAction } from '@/types/type';
+import { LOGIN_SUCCESS, LOGOUT } from '../actions/user';
 
-interface UserState {
-  id: string;
-  name: string;
-  mail: string;
-  login: boolean;
-}
-
-const initialState: UserState = {
-  id: '',
-  name: '',
-  mail: '',
-  login: false,
+const initialState = {
+  user: {
+    id: '',
+    name: '',
+    mail: '',
+    login: false,
+  },
 };
 
-type UserActionTypes = SetLoggedInUserAction; // 다른 액션들도 있다면 union(|)으로 추가
-
-const userReducer = (state = initialState, action: UserActionTypes): UserState => {
+const userReducer = (state = initialState, action: UserAction) => {
   switch (action.type) {
-    case SET_LOGGED_IN_USER:
+    case LOGIN_SUCCESS:
       return {
         ...state,
-        id: action.payload.id,
-        name: action.payload.name,
-        mail: action.payload.mail,
-        login: true,
+        user: {
+          id: action.payload.id,
+          name: action.payload.name,
+          mail: action.payload.mail,
+          login: true,
+        },
+      };
+    case LOGOUT:
+      return {
+        ...state,
+        user: {
+          ...initialState.user,
+        },
       };
     default:
       return state;
