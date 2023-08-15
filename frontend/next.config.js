f5253eb9c167c4a94b13764188643c22c1dc2c78
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-08-15 22:03:47 +0900

    feat: editor mounted error 해결을 위한 mode변경 (임시)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  reactStrictMode: false,
+};
 
-module.exports = nextConfig
+// strictMode true,
+// Editor mounted error
+// 아직까지 해결안됨 git community에서도
+// nextjs에서 draftjs 사용 시 mounted 에러 발생 사용자 다수
+
+module.exports = nextConfig;
