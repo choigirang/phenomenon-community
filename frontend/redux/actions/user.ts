69556e2f735ee65512cad59c2e8f00049f21a8f6
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-08-15 22:04:55 +0900

    feat: user 정보 저장 action

--- frontend/redux/actions/user.ts
+++ frontend/redux/actions/user.ts
@@ -1,13 +1,14 @@
-import { UserType } from '@/types/type';
+import { User } from '@/types/type';
 
-export const SET_LOGGED_IN_USER = 'SET_LOGGED_IN_USER';
+// actions.js
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGOUT = 'LOGOUT';
 
-export interface SetLoggedInUserAction {
-  type: typeof SET_LOGGED_IN_USER;
-  payload: UserType;
-}
+export const loginSuccess = (userInfo: User) => ({
+  type: LOGIN_SUCCESS,
+  payload: userInfo,
+});
 
-export const setLoggedInUser = (userData: UserType): SetLoggedInUserAction => ({
-  type: SET_LOGGED_IN_USER,
-  payload: userData,
+export const logout = () => ({
+  type: LOGOUT,
 });
