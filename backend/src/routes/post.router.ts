aa28dda62bc0bd60cf8b7f2e9347169fa13fa365
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-08-17 20:32:46 +0900

    feat: 게시글 조회 라우터 추가

--- backend/src/routes/post.router.ts
+++ backend/src/routes/post.router.ts
@@ -1,8 +1,15 @@
 import express from 'express';
-import { addPost } from '../controllers/posts.controller';
+import { addPost, showEachPost, showPostsByPage } from '../controllers/posts.controller';
 
 const postRouter = express.Router();
 
+// 게시글 조회
+postRouter.get('/all-posts', showPostsByPage);
+
+// 개별 게시글 조회
+postRouter.get('/post/:id', showEachPost);
+
+// 게시글 추가
 postRouter.post('/posts', addPost);
 
 export default postRouter;
