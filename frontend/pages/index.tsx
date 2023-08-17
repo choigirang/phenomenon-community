7d1cca822379e1ea013f94231e3dd66a9ab59901
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-08-17 20:35:08 +0900

    feat: LeftBox => PostList 변경

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,11 @@
 import Head from 'next/head';
 import axios from 'axios';
 
-import LeftBox from '../components/Common/LeftBox';
 import RightBox from '@/components/Common/RightBox';
 
 import styled from 'styled-components';
 import { Container } from '@/styles/GlobalComponents';
+import PostList from '@/components/Community/PostList';
 
 export default function Home() {
   return (
@@ -14,7 +14,7 @@ export default function Home() {
         <title>이왜진</title>
       </Head>
       <Container>
-        <LeftBox />
+        <PostList />
         <RightBox />
       </Container>
     </>
