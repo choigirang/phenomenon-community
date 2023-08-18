6cf908d5a6dc4e23b5ddc1408f1d918a54c28193
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-08-18 19:02:40 +0900

    feat: 주석 추가

--- frontend/components/Community/EachPost.tsx
+++ frontend/components/Community/EachPost.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { AiFillEye, AiFillLike, AiOutlineComment } from 'react-icons/ai';
 import styled from 'styled-components';
 
+/** 홈 화면 시 개별 게시글 목록 */
 export default function EachPost(props: EachPostProps) {
   const { postNumber, author, title, body, date, views, likes, comments } = props.posts;
 
