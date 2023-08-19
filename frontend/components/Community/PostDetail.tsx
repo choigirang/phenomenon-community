e96068a0ba7b51bbc22e81d4d94d84a35d77d70a
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-08-19 19:04:46 +0900

    feat: 개별 게시글 컴포넌트

--- frontend/components/Community/PostDetail.tsx
+++ frontend/components/Community/PostDetail.tsx
@@ -1,15 +1,23 @@
-import { api } from '@/util/api';
-import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
-import { useQuery } from 'react-query';
-import styled from 'styled-components';
-import { PostType } from '@/types/type';
+import { useRouter } from 'next/router';
+import { api } from '@/util/api';
+
+import ShowWritingData from './ShowWritingData';
+import PostComment from './PostComment';
 import { usePostDetail } from '@/hooks/usePostDetail';
+import { PostType } from '@/types/type';
 
+import styled from 'styled-components';
+import AddComment from './AddComment';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/redux/store';
+
+/** 개별 게시글 페이지 */
 export default function PostDetail({ id }: { id: number }) {
+  // 게시글 받아오기
   const queryResult = usePostDetail(id);
-
-  console.log(queryResult);
+  // 로그인 상태 확인 (댓글 기능)
+  const user = useSelector((state: RootState) => state.user.user);
 
   if (queryResult.isLoading) {
     return <div>Loading...</div>;
@@ -24,13 +32,70 @@ export default function PostDetail({ id }: { id: number }) {
     return <div>No data available</div>;
   }
 
+  // 게시글 데이터
   const data: PostType = queryResult.data;
 
-  console.log(data);
-
-  return <Container></Container>;
+  return (
+    <Container>
+      <PostInfo>
+        {/* 카테고리 들어갈 자리 */}
+        {/* 작성자, 타이틀, 날짜, 조회수 등 */}
+        <div className="title">{data.title}</div>
+        <div className="info-bottom">
+          <div className="author-date">
+            <span className="author">{data.author}</span>|<span className="date">{data.date}</span>
+          </div>
+          <div className="views-likes">
+            <span className="views">조회수 : {data.views}</span>|<span className="likes">스크랩 : {data.likes}</span>
+          </div>
+        </div>
+      </PostInfo>
+      {/* 작성한 데이터 */}
+      <ShowWritingData data={data.body} />
+      {/* 댓글 작성하기 */}
+      {<AddComment postNumber={data.postNumber} author={user.id} />}
+      {/* 댓글 */}
+      {data.comments.map((comment, idx) => (
+        <PostComment key={idx} comment={comment} />
+      ))}
+    </Container>
+  );
 }
 
 const Container = styled.div`
-  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: var(--padding-solo) 0;
+`;
+
+const PostInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: var(--margin-solo);
+  font-size: var(--size-text);
+
+  .title {
+    font-size: var(--size-title);
+    font-weight: 400;
+  }
+
+  .info-bottom {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .author-date {
+    display: flex;
+    gap: 5px;
+  }
+
+  .author {
+    font-weight: 400;
+  }
+
+  .views-likes {
+    display: flex;
+    gap: 5px;
+  }
 `;
