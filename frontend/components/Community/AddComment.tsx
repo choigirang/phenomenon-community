fec5a0ab0db286e7701e4d763343f654e3701119
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-08-19 19:02:47 +0900

    feat: 게시글 별 댓글 추가 컴포넌트

---
+++ frontend/components/Community/AddComment.tsx
@@ -0,0 +1,46 @@
+import useInputs from '@/hooks/useInputs';
+import { RootState } from '@/redux/store';
+import { api } from '@/util/api';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+
+/** 댓글 추가하기 */
+export default function AddComment({ postNumber, author }: { postNumber: number; author: string }) {
+  const [content, onChange] = useInputs('');
+
+  const user = useSelector((state: RootState) => state.user.user);
+
+  console.log(user);
+
+  const fetchCommentData = () => {
+    if (content) {
+      const postData = { postNumber, author, content };
+      api.post('/post/comment', postData).then(res => console.log(res));
+    }
+  };
+
+  return (
+    <Container>
+      <Input onChange={onChange} />
+      <button onClick={fetchCommentData}>제출</button>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  width: 100%;
+  min-height: 100px;
+  display: flex;
+  flex-direction: column;
+  border: var(--border-solid1) black;
+  border-radius: 3px;
+  padding: var(--padding-content);
+  background-color: var(--color-light-gray);
+`;
+
+const Input = styled.input`
+  width: 100%;
+  height: 100%;
+  border-radius: 3px;
+`;
