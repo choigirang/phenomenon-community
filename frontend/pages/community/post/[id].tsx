43ad1b8cf697417a321b5efd6d4688614b213947
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-08-19 19:07:36 +0900

    feat: GlobalContainer => 개별 Container

--- frontend/pages/community/post/[id].tsx
+++ frontend/pages/community/post/[id].tsx
@@ -1,10 +1,12 @@
-import { Container } from '@/styles/GlobalComponents';
 import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+
 import Login from '@/components/Common/Login';
 import PostDetail from '@/components/Community/PostDetail';
-import { useRouter } from 'next/router';
 import { usePostDetail } from '@/hooks/usePostDetail';
 import { PostType } from '@/types/type';
+
+import styled from 'styled-components';
 
 export default function index() {
   const [postNum, setPostNum] = useState<number | undefined>();
@@ -15,10 +17,9 @@ export default function index() {
     setPostNum(id);
   }, [router]);
 
-  return (
-    <Container>
-      {postNum && <PostDetail id={postNum} />}
-      <Login />
-    </Container>
-  );
+  return <Container>{postNum && <PostDetail id={postNum} />}</Container>;
 }
+
+const Container = styled.div`
+  width: 100%;
+`;
