2c098ffeb5504dce233102514674e8ed31c0871b
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-08-21 17:33:39 +0900

    feat: 로그인한 유저에 따른 작성 데이터 fetch

--- frontend/pages/my/index.tsx
+++ frontend/pages/my/index.tsx
@@ -4,6 +4,9 @@ import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
+import UserCard from '../../components/User/UserCard';
+import { useDispatch } from 'react-redux';
+import { loginSuccess } from '@/redux/actions/user';
 
 const userFetchData = async (user: string) => {
   const res = await api.get(`/my=${user}`);
@@ -14,6 +17,7 @@ export default function index() {
   const [data, setData] = useState();
 
   const user = useSelector((state: RootState) => state.user.user);
+  const dispatch = useDispatch();
 
   const router = useRouter();
 
@@ -21,15 +25,26 @@ export default function index() {
     if (user.login) {
       const fetchData = async () => {
         const result = await userFetchData(user.id);
-        console.log(result);
+        setData(result);
       };
       fetchData();
     } else {
-      router.push('/');
-    }
-  }, [user.id]);
+      /** redux 초기화 임시 */
+      const localUserData = localStorage.getItem('user');
 
-  return <Container>{data}</Container>;
+      // 초기화 시, 저장 데이터 있으면 redux 상태저장
+      // 없으면 홈 이동
+
+      if (localUserData) dispatch(loginSuccess(JSON.parse(localUserData)));
+      else router.push('/');
+    }
+  }, [router]);
+
+  return (
+    <Container>
+      <UserCard />
+    </Container>
+  );
 }
 
 const Container = styled.div`
