5f02b05b0fc0f9d3fa0f62c3200b7d46f35bdc65
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-08-21 17:33:15 +0900

    fea: 임시 콘솔 값 확인

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -7,8 +7,12 @@ import PostList from '@/components/Community/PostList';
 
 import styled from 'styled-components';
 import { Container } from '@/styles/GlobalComponents';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/redux/store';
 
 export default function Home() {
+  const user = useSelector((state: RootState) => state.user.user);
+
   return (
     <>
       <Head>
