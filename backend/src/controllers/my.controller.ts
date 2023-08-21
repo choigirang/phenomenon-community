0698a7d94ed11f307acdf7fcecc989fdb0fa3af5
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-08-21 17:30:35 +0900

    feat: user가 작성한 data send

--- backend/src/controllers/my.controller.ts
+++ backend/src/controllers/my.controller.ts
@@ -1,13 +1,18 @@
 import { Request, Response } from 'express';
+import Post from '../models/posts.model';
+import { PostType } from '../../type/type';
+import { CommentData } from '../../type/type';
 
 export async function userAllData(req: Request, res: Response) {
   const user = req.params.user;
-  // Post와 Comment에서 찾기
-  // user의 likes 저장하기
-  //   const
+
+  const userPosts = await Post.find({ author: user });
+
+  // 해당 user가 작성한 모든 포스트의 comments에서 해당 user의 데이터 가져오기
+  const userComments = await Post.find({ 'comments.author': user });
 
   try {
-    return res.status(200).send({ user });
+    return res.status(200).send({ user, userPosts, userComments });
   } catch (err) {
     return res.status(404).send(err);
   }
