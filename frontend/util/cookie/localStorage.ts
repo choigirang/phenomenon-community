0719f474be2fcaf1d5a904514caa57c37ce64900
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-08-21 17:34:17 +0900

    feat: login data save localstorage

--- frontend/util/cookie/localStorage.ts
+++ frontend/util/cookie/localStorage.ts
@@ -1,3 +1,5 @@
+import { User } from '@/types/type';
+
 export function setToken(accessToken: string, refreshToken: string) {
   localStorage.setItem('accessToken', accessToken);
   localStorage.setItem('refreshToken', refreshToken);
@@ -6,4 +8,12 @@ export function setToken(accessToken: string, refreshToken: string) {
 export function deleteToken() {
   localStorage.removeItem('accessToken');
   localStorage.removeItem('refreshToken');
+}
+
+export function saveLoginData(user: User) {
+  localStorage.setItem('user', JSON.stringify(user));
+}
+
+export function deleteLoginData() {
+  localStorage.removeItem('user');
 }
