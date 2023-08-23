1da9a694003efc1669f285f01a05299d1461a1fe
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-08-23 23:26:33 +0900

    feat: comment props 변경

--- frontend/components/User/UserData.tsx
+++ frontend/components/User/UserData.tsx
@@ -1,4 +1,4 @@
-import { CommentType, PostType, User, UserDataLogType } from '@/types/type';
+import { CommentType, PostType, User, UserCommentsMap, UserDataLogType } from '@/types/type';
 import { api } from '@/util/api';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
@@ -8,7 +8,7 @@ import useDataLog from '@/hooks/user/useDataLog';
 
 export default function UserData({ data }: { data: User }) {
   const [postsData, setPostsData] = useState<PostType[]>();
-  const [commentsData, setCommentsData] = useState<CommentType[]>();
+  const [commentsData, setCommentsData] = useState<UserCommentsMap>();
   const { id, name, mail } = data;
 
   const userDataLogQuery = useDataLog(id);
@@ -16,30 +16,40 @@ export default function UserData({ data }: { data: User }) {
   useEffect(() => {
     // userData가 로드된 후에 userPosts와 userComments
     if (userDataLogQuery.data) {
-      const { userPosts, userComments }: UserDataLogType = userDataLogQuery.data;
-      setPostsData(userPosts);
-      setCommentsData(userComments);
+      const { userPosts, userCommentsMap }: UserDataLogType = userDataLogQuery.data;
+      setPostsData(userPosts.sort());
+      setCommentsData(userCommentsMap);
     }
   }, [userDataLogQuery.data]);
+
+  console.log(commentsData);
+
   return (
     <Container>
       <List>
         <p className="sub-title">내가 작성한 글</p>
         {postsData && postsData.map(post => <UserPostData key={post.postNumber} post={post} />)}
       </List>
-      <ul className="list">
-        {commentsData && commentsData.map((comment, idx) => <UserCommentData key={idx} comment={comment} />)}
-      </ul>
+      <List>
+        <p className="sub-title">내가 작성한 댓글</p>
+        {commentsData &&
+          Object.keys(commentsData).map(key => (
+            <UserCommentData key={key} postNumber={key} comment={commentsData[key]} />
+          ))}
+      </List>
     </Container>
   );
 }
 
 const Container = styled.div`
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
   border: solid 1px var(--color-dark-gray);
   border-radius: 5px;
   background-color: var(--color-dark-white);
-  padding: var(--padding-content);
+  padding: var(--padding-side);
 `;
 
 const List = styled.ul`
