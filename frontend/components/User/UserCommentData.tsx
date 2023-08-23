d22a03dd01922da70f6a8b595846471b4403639b
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-08-23 23:26:11 +0900

    feat: 임시 주석 처리

--- frontend/components/User/UserCommentData.tsx
+++ frontend/components/User/UserCommentData.tsx
@@ -1,10 +1,55 @@
-import { CommentType } from '@/types/type';
-import React from 'react';
+import { CommentProps, CommentType } from '@/types/type';
+import Link from 'next/link';
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
 
-export default function UserCommentData({ comment }: { comment: CommentType }) {
-  return <div>UserCommentData</div>;
+export default function UserCommentData({ postNumber, comment }: CommentProps) {
+  const viewContainerRef = React.useRef<HTMLDivElement>(null);
+
+  console.log(comment);
+
+  return (
+    <Container>
+      {/* <Linked href={`/community/post/${post.postNumber}`}>
+        <Top>
+          <p className="title">{comment}</p>
+          <p className="date">{date}</p>
+        </Top>
+      <div className="body" ref={viewContainerRef} />
+      </Linked> */}
+    </Container>
+  );
 }
 
-// author: data.author, comment : data.comment
-// {...data} 위랑 샘샘
-// {data} : {data: CommentType}
+const Container = styled.li`
+  height: 100px;
+  background-color: white;
+  border-radius: 5px;
+  box-shadow: 2px 2px 2px 1px rgba(129, 129, 250, 0.2);
+  padding: var(--padding-content);
+  font-size: var(--size-text);
+
+  .title {
+    font-weight: 400;
+    color: var(--color-dark-gray);
+  }
+
+  .body {
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+  }
+`;
+
+const Linked = styled(Link)`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const Top = styled.div`
+  display: flex;
+  gap: 10px;
+`;
