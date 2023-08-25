2474a51a6043cdf8ad16645d89cc79833cc4a916
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-08-25 19:04:17 +0900

    feat: notice query res data 전처리 에정

--- frontend/components/Notice/NoticeDetail.tsx
+++ frontend/components/Notice/NoticeDetail.tsx
@@ -1,8 +1,25 @@
 import { useNoticeDetail } from '@/hooks/notice/useNoticeDetail';
+import { Notice } from '@/types/type';
 import React from 'react';
 
 export default function NoticeDetail({ id }: { id: number }) {
   const queryResult = useNoticeDetail(id);
+
+  if (queryResult.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (queryResult.isError) {
+    const error = queryResult.error as Error;
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (!queryResult.data) {
+    return <div>No data available</div>;
+  }
+
+  // 게시글 데이터
+  const data: Notice = queryResult.data;
 
   return <div>NoticeDetail</div>;
 }
