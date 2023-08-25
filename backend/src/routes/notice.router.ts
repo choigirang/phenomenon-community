86303ed89a579025691214992c0bf622744807c2
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-08-25 19:03:37 +0900

    feat: 공지사항 개별 조회 추가

--- backend/src/routes/notice.router.ts
+++ backend/src/routes/notice.router.ts
@@ -1,10 +1,13 @@
 import express from 'express';
-import { addNotice, showNotice } from '../controllers/notice.controller';
+import { addNotice, eachNotice, showNotice } from '../controllers/notice.controller';
 
 export const noticeRouter = express.Router();
 
-/** 공지사항 조회 */
+/** 공지사항 전체 조회 */
 noticeRouter.get('/notice', showNotice);
+
+/** 공지사항 개별 조회*/
+noticeRouter.get('/notice/:id', eachNotice);
 
 /** 공지사항 추가 */
 noticeRouter.post('/notice', addNotice);
