717fce6bff0ccbf61ba0800b2ef0538d6c06292f
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-08-25 18:22:13 +0900

    feat: create notice schema

---
+++ backend/src/models/notice.model.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from 'mongoose';
+import { Notice } from '../../type/type';
+
+const noticeSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  noticeNumber: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Notice = mongoose.model<Notice>('Notice', noticeSchema);
+
+export default Notice;
