e788289087c30995067bc7bf6803fee50bb0d04f
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-08-26 21:03:10 +0900

    feat: add likes, find 수정 필요

--- frontend/components/Community/PostDetail.tsx
+++ frontend/components/Community/PostDetail.tsx
@@ -11,13 +11,22 @@ import { useSelector } from 'react-redux';
 import { RootState } from '@/redux/store';
 import EachComment from './EachComment';
 import { usePostDetail } from '@/hooks/post/usePostDetail';
+import { AiFillLike, AiOutlineLike } from 'react-icons/ai';
+import { useDispatch } from 'react-redux';
+import { loginSuccess } from '@/redux/actions/user';
 
 /** 개별 게시글 페이지 */
 export default function PostDetail({ id }: { id: number }) {
+  const [likes, setLikes] = useState<boolean>();
   // 게시글 받아오기
   const queryResult = usePostDetail(id);
   // 로그인 상태 확인 (댓글 기능)
   const user = useSelector((state: RootState) => state.user.user);
+  const dispatch = useDispatch();
+
+  // const findLike = user.likes.find(like => like.postNumber === id);
+  // if (findLike) setLikes(true);
+  // else setLikes(false);
 
   if (queryResult.isLoading) {
     return <div>Loading...</div>;
@@ -35,12 +44,23 @@ export default function PostDetail({ id }: { id: number }) {
   // 게시글 데이터
   const data: PostType = queryResult.data;
 
+  const likesHadnler = () => {
+    api.post('/likes', { id: user.id, postNumber: id }).then(res => {
+      dispatch(loginSuccess(res.data));
+    });
+  };
+
   return (
     <Container>
       <PostInfo>
         {/* 카테고리 들어갈 자리 */}
         {/* 작성자, 타이틀, 날짜, 조회수 등 */}
-        <div className="title">{data.title}</div>
+        <div className="info-top">
+          <p className="title">{data.title}</p>
+          <p className="likse" onClick={likesHadnler}>
+            {likes ? <AiFillLike size="18px" /> : <AiOutlineLike size="18px" />}
+          </p>
+        </div>
         <div className="info-bottom">
           <div className="author-date">
             <span className="author">{data.author}</span>|<span className="date">{data.date}</span>
@@ -75,9 +95,19 @@ const PostInfo = styled.div`
   gap: var(--margin-solo);
   font-size: var(--size-text);
 
+  .info-top {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
   .title {
     font-size: var(--size-title);
     font-weight: 400;
+  }
+
+  .likse {
+    cursor: pointer;
   }
 
   .info-bottom {
