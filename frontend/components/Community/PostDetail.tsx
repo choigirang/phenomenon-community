69921e504b291579bd15951eabe8c38d518d943f
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-08-27 19:55:14 +0900

    feat: 서버,클라이언트 구현 완료, 아이콘 변경을 위한  filter 안됨

--- frontend/components/Community/PostDetail.tsx
+++ frontend/components/Community/PostDetail.tsx
@@ -28,6 +28,15 @@ export default function PostDetail({ id }: { id: number }) {
   // if (findLike) setLikes(true);
   // else setLikes(false);
 
+  // useEffect(() => {
+  //   if (user) {
+  //     const checkLike = user.likes.filter(like => like.postNumber === id);
+  //     setLikes(checkLike.length > 0);
+  //   } else {
+  //     setLikes(false); // user가 없을 때 likes를 false로 설정
+  //   }
+  // }, [user, id]);
+
   if (queryResult.isLoading) {
     return <div>Loading...</div>;
   }
