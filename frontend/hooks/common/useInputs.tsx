c65e0cd4955b213586e93958cf438e99c21590a3
GiRang Choi <choigirang@kakao.com>
2023-08-29 13:52:08 +0900

    feat: useInput 매개변수 타입 지정

--- frontend/hooks/common/useInputs.tsx
+++ frontend/hooks/common/useInputs.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 // 초깃값 없을 시 2중 에러
 export default function useInputs(initialData: string | number) {
-  const [data, setData] = useState(initialData);
+  const [data, setData] = useState<string | number>(initialData);
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const {
