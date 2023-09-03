51f9b150d377ac79e9107bc1540d610a38f0ef1c
GiRang Choi <choigirang@kakao.com>
2023-09-04 00:42:12 +0900

    feat: 유저 검색 페이지

--- frontend/pages/user/index.tsx
+++ frontend/pages/user/index.tsx
@@ -2,37 +2,123 @@ import { api } from '@/util/api';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Link from 'next/link';
+import useInputs from '@/hooks/common/useInputs';
+import { BsArrowRepeat } from 'react-icons/bs';
+import useUserName from '@/hooks/user/useUserName';
 
 export default function index() {
+  // 검색한 유저 데이터
   const [users, setUsers] = useState<string[]>([]);
+  // 검색값
+  const [userName, setUserName] = useInputs('');
+  // 검색 내용
+  const [msg, setMsg] = useState<string | number>('');
+  // 유저 쿼리
+  const queryResult = useUserName('all');
 
+  console.log(queryResult);
+
+  // 전체 유저 데이터
   useEffect(() => {
-    api.get('/users').then(res => {
-      setUsers(res.data.usersData);
-      console.log(res.data);
-    });
+    // setUsers(queryResult);
   }, []);
+
+  // enter 눌렀을 시 검색
+  function searchKeybordHandler(e: React.KeyboardEvent<HTMLElement>) {
+    if (e.key === 'Enter') {
+      if (!userName) return alert('검색어가 필요합니다.');
+      e.preventDefault();
+
+      api.get(`/user?id=${userName}`).then(res => setUsers(res.data.userData));
+      setMsg(userName);
+    }
+  }
+
+  // 검색 눌렀을 시 검색
+  function searchMouseHandler(e: React.MouseEvent<HTMLButtonElement>) {
+    if (!userName) return alert('검색어가 필요합니다.');
+    api.get(`/user?id=${userName}`).then(res => setUsers(res.data.userData));
+    setMsg(userName);
+  }
 
   return (
     <Container>
-      {users &&
-        users.map(user => (
-          <UserCard key={user} href={`/user/${user}`}>
-            {user}
-          </UserCard>
-        ))}
+      {/* 유저 검색 */}
+      <InputBox>
+        <Inputs
+          placeholder="게시글 통합 검색"
+          value={userName}
+          onChange={setUserName}
+          onKeyDown={searchKeybordHandler}></Inputs>
+        <button type="button" color="var(--color-blue)" onClick={searchMouseHandler}>
+          검색
+        </button>
+        {/* <BsArrowRepeat onClick={}/> */}
+      </InputBox>
+      {/* 검색 결과 */}
+      {msg && (
+        <p className="title">
+          "<span className="keyword">{msg}</span>" 로 검색한 결과입니다.
+        </p>
+      )}
+      {/* 유저 목록 */}
+      <UserBox>
+        {users &&
+          users.map(user => (
+            <UserCard key={user} href={`/user/${user}`}>
+              {user}
+            </UserCard>
+          ))}
+      </UserBox>
     </Container>
   );
 }
 
 const Container = styled.div`
-  height: 150px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const InputBox = styled.div`
+  display: flex;
+  height: 40px;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  margin: var(--margin-solo) 0;
+
+  button {
+    height: 100%;
+    padding: var(--padding-text);
+    border-radius: 3px;
+    background-color: var(--color-dark-gray);
+    color: white;
+
+    :hover {
+      background-color: var(--color-light-gray);
+    }
+  }
+
+  .keyword {
+    font-weight: 500;
+  }
+`;
+
+const Inputs = styled.input`
+  width: 50%;
+  height: 100%;
+  border: var(--border-solid1) var(--color-dark-blue);
+  border-radius: 3px;
+  padding: var(--padding-text);
+`;
+
+const UserBox = styled.div`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
-  gap: 10px;
 `;
 
 const UserCard = styled(Link)`
+  height: 150px;
   display: flex;
   align-items: center;
   justify-content: center;
