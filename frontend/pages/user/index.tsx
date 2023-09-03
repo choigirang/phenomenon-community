439d094f91424813097dd552e34f4bb0660fb45f
GiRang Choi <choigirang@kakao.com>
2023-09-03 15:54:02 +0900

    feat: User 검색 페이지

--- frontend/pages/user/index.tsx
+++ frontend/pages/user/index.tsx
@@ -1,17 +1,44 @@
 import { api } from '@/util/api';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import Link from 'next/link';
 
 export default function index() {
-  const [users, setUsers] = useState();
+  const [users, setUsers] = useState<string[]>([]);
 
   useEffect(() => {
-    api.get('/users').then(res => setUsers(res.data));
+    api.get('/users').then(res => {
+      setUsers(res.data.usersData);
+      console.log(res.data);
+    });
   }, []);
 
-  console.log(users);
-
-  return <Container></Container>;
+  return (
+    <Container>
+      {users &&
+        users.map(user => (
+          <UserCard key={user} href={`/user/${user}`}>
+            {user}
+          </UserCard>
+        ))}
+    </Container>
+  );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  height: 150px;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 10px;
+`;
+
+const UserCard = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: var(--padding-content);
+  border: var(--border-solid1) var(--color-dark-blue);
+  border-radius: 3px;
+  box-shadow: 3px var(--color-gray);
+`;
