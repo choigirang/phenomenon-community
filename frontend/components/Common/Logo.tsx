30dffcc540ea8006e98e617f99b7abec2c440b35
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-09-04 21:07:37 +0900

    feat: 임시 이미지 지정

--- frontend/components/Common/Logo.tsx
+++ frontend/components/Common/Logo.tsx
@@ -1,7 +1,12 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
 
 export default function Logo() {
-  return <Link href={'/'}>Logo</Link>;
+  return (
+    <Link href={'/'}>
+      <Image src={''} alt="example" width={40} height={40}></Image>
+    </Link>
+  );
 }
