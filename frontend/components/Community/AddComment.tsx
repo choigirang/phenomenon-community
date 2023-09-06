db59a140a27cb6977c033a560c0a937ddbbceb4f
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-09-06 20:08:29 +0900

    feat: useInput => textarea

--- frontend/components/Community/AddComment.tsx
+++ frontend/components/Community/AddComment.tsx
@@ -2,13 +2,14 @@ import usePostForm from '@/hooks/post/usePostForm';
 import useInputs from '@/hooks/common/useInputs';
 import { RootState } from '@/redux/store';
 import { api } from '@/util/api';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 
 /** 댓글 추가하기 */
 export default function AddComment({ postNumber, author }: { postNumber: number; author: string }) {
-  const [content, onChange] = useInputs('');
+  const [content, setContent] = useState('');
+
   const { dateHandler } = usePostForm();
 
   const user = useSelector((state: RootState) => state.user.user);
@@ -17,13 +18,17 @@ export default function AddComment({ postNumber, author }: { postNumber: number;
     if (content) {
       const postData = { postNumber, author, comment: content, date: dateHandler() };
       api.post('/post/comment', postData).then(res => console.log(res));
+      setContent('');
     }
-    console.log({ date: dateHandler() });
   };
 
   return (
     <Container>
-      <Input onChange={onChange} />
+      <textarea
+        rows={4} // 원하는 행의 수를 지정합니다.
+        value={content}
+        onChange={e => setContent(e.target.value)}
+      />
       <button onClick={fetchCommentData} disabled={!user.login}>
         제출
       </button>
@@ -35,15 +40,25 @@ const Container = styled.div`
   width: 100%;
   min-height: 100px;
   display: flex;
-  flex-direction: column;
+  gap: 20px;
   border: var(--border-solid1) black;
   border-radius: 3px;
   padding: var(--padding-content);
-  background-color: var(--color-light-gray);
-`;
+  background-color: var(--color-dark-white);
 
-const Input = styled.input`
-  width: 100%;
-  height: 100%;
-  border-radius: 3px;
+  textarea {
+    width: 100%;
+    padding: var(--padding-text);
+  }
+
+  button {
+    width: 10%;
+    background-color: var(--color-light-gray);
+    border-radius: 5px;
+
+    :hover {
+      background-color: var(--color-blue);
+      color: white;
+    }
+  }
 `;
