53c3d2d9660d7a463c3b7a5597541553b327d49d
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-09-06 14:30:35 +0900

    feat: community 페이지 카테고리 및 전체 포스터

--- frontend/pages/community/index.tsx
+++ frontend/pages/community/index.tsx
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Login from '@/components/Common/Login';
 
 import { Container } from '@/styles/GlobalComponents';
 import styled from 'styled-components';
+import Category from '../../components/Common/Category';
+import usePostAll from '../../hooks/post/usePostAll';
+import { useRouter } from 'next/router';
+import { PostType } from '@/types/type';
+import { api } from '@/util/api';
+import EachPost from '@/components/Community/EachPost';
+import { CATEGORY } from '@/constant/constant';
 
 export default function index() {
+  const [posts, setPosts] = useState<PostType[]>([]);
+  const router = useRouter();
+  let category = router.query.category as string;
+
+  // 카테고리별 게시글 받아오기
+  useEffect(() => {
+    if (!category) category = 'all';
+    api.get(`/posts/${category}`).then(res => {
+      setPosts(res.data);
+    });
+  }, [router.query]);
+
+  // 카테고리별
+  function findKeyByValue(obj: { [key: string]: string }, value: string) {
+    const keys = Object.keys(obj);
+    const foundKey = keys.find(key => obj[key] === value);
+    return foundKey || null; // 원하는 value를 찾지 못한 경우
+  }
+
+  const findCategory = findKeyByValue(CATEGORY, category);
+
   return (
     <Container>
       <CommunityContainer>
+        <Category />
         <BestPost>
-          <p className="sub-title">개념글</p>
+          <p className="sub-title">{findCategory} 전체보기</p>
         </BestPost>
+        {posts.map(post => (
+          <EachPost key={post.postNumber} item={post} />
+        ))}
       </CommunityContainer>
       <Login />
     </Container>
@@ -23,6 +55,7 @@ const CommunityContainer = styled.div`
   flex-direction: column;
   padding: var(--padding-content);
   padding-left: 0;
+  padding-top: 0;
 `;
 
 const BestPost = styled.div`
@@ -30,5 +63,6 @@ const BestPost = styled.div`
 
   .sub-title {
     border-bottom: var(--border-solid1) var(--color-blue);
+    margin: var(--padding-side) 0 0;
   }
 `;
