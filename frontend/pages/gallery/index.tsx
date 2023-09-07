f35d5176b4d12088efa597d819c637f09dbe2341
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-09-07 20:56:50 +0900

    chore: 임시 스타일 적용 Container

--- frontend/pages/gallery/index.tsx
+++ frontend/pages/gallery/index.tsx
@@ -1,6 +1,5 @@
 import Login from '@/components/Common/Login';
 import GalleryItem from '@/components/Gallery/GalleryItem';
-import { Container } from '@/styles/GlobalComponents';
 import { GalleryData } from '@/types/type';
 import { api } from '@/util/api';
 import React, { useEffect, useState } from 'react';
@@ -21,8 +20,18 @@ export default function index() {
   );
 }
 
+const Container = styled.div`
+  padding: 0 calc((100% - 1280px) / 2);
+  padding-top: 10px;
+  display: grid;
+  grid-template-columns: calc(100% - 300px) 300px;
+  grid-row-end: auto;
+  justify-content: space-between;
+`;
+
 const GalleryList = styled.ul`
   width: 100%;
   display: grid;
   grid-template-columns: repeat(5, 1fr);
+  gap: 10px;
 `;
