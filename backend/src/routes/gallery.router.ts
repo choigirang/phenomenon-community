217b64d151307138b0dae949ea09ec75a8218c7d
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-09-07 20:52:21 +0900

    feat: 갤러리 최근 조회, 개별 게시글 조회, 갤러리 삭제, 갤러리 좋아요 라우터 추가

--- backend/src/routes/gallery.router.ts
+++ backend/src/routes/gallery.router.ts
@@ -1,11 +1,29 @@
 import express from 'express';
 import { upload } from '../config/multer';
-import { addImageToGallery, galleryList } from '../controllers/gallery.controller';
+import {
+  addImageToGallery,
+  deleteGallery,
+  detailGallery,
+  galleryList,
+  latestGallery,
+  likesGallery,
+} from '../controllers/gallery.controller';
 
 export const galleryRouter = express.Router();
+
+// 갤러리 최근 조회
+galleryRouter.get('/gallery/latest', latestGallery);
 
 // 갤러리 조회
 galleryRouter.get('/gallery', galleryList);
 
 // 갤러리 이미지 추가
 galleryRouter.post('/gallery', upload('gallery').array('images', 10), addImageToGallery);
+
+// 개별 갤러리 조회
+galleryRouter.get('/gallery/:id', detailGallery);
+
+galleryRouter.delete('/gallery/:id', deleteGallery);
+
+// 갤러리 좋아요
+galleryRouter.post('/gallery/likes', likesGallery);
