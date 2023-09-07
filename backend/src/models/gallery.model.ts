4b6c57751286d0089490f88662230483b556aa96
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-09-07 17:42:39 +0900

    feat: gallery.model

---
+++ backend/src/models/gallery.model.ts
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+import { Gallery } from '../../type/type';
+
+const gallerySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  galleryNumber: {
+    type: Number,
+    required: true,
+  },
+  images: [
+    {
+      src: {
+        type: String,
+      },
+    },
+  ],
+});
+
+const Gallery = mongoose.model<Gallery>('Gallery', gallerySchema);
+
+export default Gallery;
