bea80a034074e03dede82961819700cdf1598c00
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-09-08 17:35:08 +0900

    feat: gallery comments 추가, 수정, 삭제 router

--- backend/src/routes/gallery.router.ts
+++ backend/src/routes/gallery.router.ts
@@ -4,6 +4,9 @@ import {
   addImageToGallery,
   deleteGallery,
   detailGallery,
+  galleryAddComment,
+  galleryDeleteComment,
+  galleryEditComment,
   galleryList,
   latestGallery,
   likesGallery,
@@ -23,7 +26,17 @@ galleryRouter.post('/gallery', upload('gallery').array('images', 10), addImageTo
 // 개별 갤러리 조회
 galleryRouter.get('/gallery/:id', detailGallery);
 
+// 갤러리 삭제
 galleryRouter.delete('/gallery/:id', deleteGallery);
 
 // 갤러리 좋아요
 galleryRouter.post('/gallery/likes', likesGallery);
+
+// 갤러리 댓글 추가
+galleryRouter.post('/gallery/comment', galleryAddComment);
+
+// 갤러리 댓글 수정
+galleryRouter.post('/gallery/:galleryNumber/comments/:commentNumber', galleryEditComment);
+
+// 갤러리 댓글 삭제
+galleryRouter.delete('/gallery/:galleryNumber/comments/:commentNumber', galleryDeleteComment);
