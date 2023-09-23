c7cf7aebe10afede2d492586b7abe3fdb3166056
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-09-23 20:48:50 +0900

    env 추가

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,20 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const mongoose_1 = __importDefault(require("mongoose"));
-const cors_1 = __importDefault(require("cors"));
-const cookie_parser_1 = __importDefault(require("cookie-parser"));
-const users_router_1 = require("./routes/users.router");
-const post_router_1 = require("./routes/post.router");
-const my_router_1 = require("./routes/my.router");
-const notice_router_1 = require("./routes/notice.router");
-const gallery_router_1 = require("./routes/gallery.router");
-const app = (0, express_1.default)();
-const port = 8080;
+var express_1 = require("express");
+var mongoose_1 = require("mongoose");
+var cors_1 = require("cors");
+var cookie_parser_1 = require("cookie-parser");
+var dotenv_1 = require("dotenv");
+var users_router_1 = require("./routes/users.router");
+var post_router_1 = require("./routes/post.router");
+var my_router_1 = require("./routes/my.router");
+var notice_router_1 = require("./routes/notice.router");
+var gallery_router_1 = require("./routes/gallery.router");
+dotenv_1.default.config();
+var server = process.env.SERVER_NAME;
+var password = process.env.SERVER_PASS;
+var app = (0, express_1.default)();
+var port = 8080;
 app.use((0, cors_1.default)({ origin: true, credentials: true }));
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
@@ -21,6 +22,6 @@ app.use(users_router_1.usersRouter, post_router_1.postRouter, my_router_1.myRout
 app.listen(port);
 // mongoDB 연결
 mongoose_1.default
-    .connect('mongodb+srv://chlrlfkd:chlrlfkd5633@phenomenon-community.zyo8dzo.mongodb.net/?retryWrites=true&w=majority')
-    .then(() => console.log('connected successfully'))
-    .catch((err) => console.log(err));
+    .connect("mongodb+srv://".concat(server, ":").concat(password, "@phenomenon-community.zyo8dzo.mongodb.net/?retryWrites=true&w=majority"))
+    .then(function () { return console.log('connected successfully'); })
+    .catch(function (err) { return console.log(err); });
