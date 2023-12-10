ae8297f2262082cb8718ad21959fad792cb0c8f4
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-12-10 22:47:31 +0900

    feat: add likes router

--- backend/src/routes/my.router.ts
+++ backend/src/routes/my.router.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { showUserComments, showUserPosts } from '../controllers/my.controller';
+import { showUserComments, showUserLikes, showUserPosts } from '../controllers/my.controller';
 // import { userAllData } from '../controllers/my.controller';
 
 export const myRouter = express.Router();
@@ -9,3 +9,5 @@ export const myRouter = express.Router();
 myRouter.get('/my/posts', showUserPosts);
 
 myRouter.get('/my/comments', showUserComments);
+
+myRouter.get('/my/likes', showUserLikes);
