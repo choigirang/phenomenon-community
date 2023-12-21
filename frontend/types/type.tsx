ca1d19a542fc173aa1f809871b44eb125a0232c4
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-12-21 23:20:40 +0900

    feat: sign page refactor에 따른 add type

--- frontend/types/type.tsx
+++ frontend/types/type.tsx
@@ -165,3 +165,47 @@ export type GalleryType = {
   images: ImageSrc[];
   comments: CommentType[];
 };
+
+/** Sign up input type */
+export interface InputType {
+  [key: string]: string;
+}
+
+/** 유효성 검사 */
+export type ValidationItem = {
+  name: string;
+  check: (data: string) => boolean;
+};
+
+/** 아이디 상태값 */
+export type CheckId = {
+  userId: string;
+  required: boolean;
+};
+
+/** 비밀번호에 따른 유효성 타입 */
+export type PassInputData = {
+  validatePass: boolean;
+  leastPass: boolean;
+  // leastNickname: boolean;
+};
+
+/** 닉네임 */
+export type CheckName = {
+  name: string;
+  checkName: boolean;
+};
+
+/** 보안 코드 */
+export interface AxiosSecurityCode {
+  code: string;
+  userCode: string;
+}
+
+/** 보안 코드 확인 */
+export type CheckSecurityType = {
+  agree: boolean;
+  compareSecurityCode: boolean;
+  errInfo: string;
+  errCode: string;
+};
