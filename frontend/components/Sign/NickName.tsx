1503e5e3d3d6f8c1a2a963dd93eedeee5d4d4436
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-12-29 18:33:53 +0900

    feat: nickName 중복 검사 추가

--- frontend/components/Sign/NickName.tsx
+++ frontend/components/Sign/NickName.tsx
@@ -1,4 +1,5 @@
 import { CheckName } from '@/types/type';
+import { api } from '@/util/api';
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
@@ -30,18 +31,30 @@ export default function NickName({ nickName, setNickName }: NickNameProps) {
     }));
   };
 
+  const checkDuplicateNickName = () => {
+    api
+      .get(`/check?nickName=${nickName}`)
+      .then(res => {
+        alert('사용할 수 있는 닉네임입니다.');
+      })
+      .catch(err => alert('사용할 수 없는 아이디입니다.'));
+  };
+
   return (
     <NickNameContainer>
       <Label htmlFor="name">닉네임</Label>
       <NameBox>
-        <input
-          id="name"
-          name="name"
-          type="text"
-          placeholder="닉네임을 입력해주세요."
-          onChange={nicknameHandler}
-          required
-        />
+        <InputBox>
+          <input
+            id="name"
+            name="name"
+            type="text"
+            placeholder="닉네임을 입력해주세요."
+            onChange={nicknameHandler}
+            required
+          />
+          <button onClick={checkDuplicateNickName}>중복검사</button>
+        </InputBox>
         <span>{`${'2~20자의 닉네임을 입력해주세요.(띄어쓰기는 허용되지 않습니다.)'}`}</span>
       </NameBox>
     </NickNameContainer>
@@ -70,5 +83,23 @@ const NameBox = styled.div`
     padding-top: var(--padding-solo);
     font-size: var(--size-text);
     // color: ${props => (props ? 'var(--color-gray)' : 'var(--color-red)')};
+  }
+`;
+
+const InputBox = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 10px;
+
+  input {
+    width: 100%;
+  }
+
+  button {
+    width: 100px;
+
+    &:hover {
+      background-color: var(--color-light-gray);
+    }
   }
 `;
