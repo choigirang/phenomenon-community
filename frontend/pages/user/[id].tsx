b21e530c9a9fec910c2e19549bdcb07a5f482ba4
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-01-04 16:25:37 +0900

    refactor: 라우터 수정 및 UserCard, UserData 활용

--- frontend/pages/user/[id].tsx
+++ frontend/pages/user/[id].tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 
 import { api } from '@/util/api';
 import UserCard from '@/components/User/UserCard';
+import UserData from '@/components/User/UserData';
 import { SearchUser } from '@/types/type';
 
 import styled from 'styled-components';
@@ -16,36 +17,20 @@ export default function UserPage() {
   const [data, setData] = useState<SearchUser>();
 
   const router = useRouter();
-  const user = router.query.id;
-  const viewContainerRef = React.useRef<HTMLDivElement>(null);
+  const { id } = router.query;
 
   useEffect(() => {
-    if (user) {
-      api.get(`/user/${user}`).then(res => setData(res.data));
+    if (id) {
+      api.get(`/user?id=${id}`).then(res => setData(res.data));
     }
-  }, [user]);
-
-  function stripHtml(html: string): string {
-    return html.replace(/<[^>]*>?/gm, '');
-  }
+  }, [id]);
 
   return (
     data && (
       <Container>
-        <UserCard data={data} />
+        <UserCard data={data.userInfo} />
         {/* 작성한 글 목록 */}
-        <List>
-          <p className="title">작성한 글</p>
-          {data.posts.map(post => (
-            <Post href={`/community/post/${post.postNumber}`} key={post.postNumber}>
-              <div className="title-date">
-                <span className="title">{post.title}</span>
-                <span className="date">{post.date}</span>
-              </div>
-              <p>{stripHtml(post.body)}</p>
-            </Post>
-          ))}
-        </List>
+        <UserData data={data} />
       </Container>
     )
   );
@@ -55,6 +40,17 @@ const Container = styled.div`
   display: grid;
   grid-template-columns: 300px calc(100% - 300px);
   gap: 20px;
+`;
+
+const ListContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  border: solid 1px var(--color-dark-gray);
+  border-radius: 5px;
+  background-color: var(--color-dark-white);
+  padding: var(--padding-side);
 `;
 
 const List = styled.div`
