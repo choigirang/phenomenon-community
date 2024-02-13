536e9dd228ebe3d07d1bdbdd2959e9ac74289345
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-02-13 23:08:32 +0900

    chore: 환경변수 나눠놓기

--- frontend/util/api/index.ts
+++ frontend/util/api/index.ts
@@ -8,7 +8,7 @@ const devEndpoint = 'http://localhost';
 const prodEndpoint = 'http://13.209.167.140';
 
 export const api = axios.create({
-  baseURL: 'http://43.201.64.212:8080/',
+  baseURL: 'http://localhost:8080/',
   // timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
