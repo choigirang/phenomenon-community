aa07053f0b2185fcc66081955f951b7427aad942
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-12 00:02:03 +0900

    refactor: default layout with provider

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import '../style/global.css';
-import Header from './(nav)/header';
+import Header from './(common)/header';
+import Footer from './(common)/footer';
+import QueryProvider from '../util/provider/queryProvider';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="kr">
       <body>
-        <Header />
-        {children}
+        <QueryProvider>
+          <Header />
+          {children}
+          <Footer />
+        </QueryProvider>
       </body>
     </html>
   );
