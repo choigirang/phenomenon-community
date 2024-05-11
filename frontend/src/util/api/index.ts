5b02f60ce13c30efcbaf2837cdc08c6e283aa18e
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-12 00:01:18 +0900

    move folder

---
+++ frontend/src/util/api/index.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { getCookie } from '../cookie';
+
+const env = process.env.REACT_APP_ENV;
+
+const devEndpoint = 'http://localhost';
+
+const prodEndpoint = 'http://13.209.167.140';
+
+export const api = axios.create({
+  baseURL: 'http://localhost:8080/',
+  // timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+});
+
+api.interceptors.request.use();
