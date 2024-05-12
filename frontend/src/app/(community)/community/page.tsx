f27120df7aa3ea80e680640a888c79233ffd9509
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-12 22:55:16 +0900

    refactor: fetching data in community page

--- frontend/src/app/(community)/community/page.tsx
+++ frontend/src/app/(community)/community/page.tsx
@@ -1,3 +1,35 @@
-export default function Page() {
-  return <div></div>;
+import { api } from '@/util/api';
+import Category from '../category';
+import { PostType } from '@/type/community/type';
+import List from '../list';
+import Link from 'next/link';
+
+async function getAllPosts(page: string) {
+  const res = await api.get(`/posts?category=all&page=${page}`);
+  return res.data;
+}
+
+export default async function Page({ params: { page } }: { params: { page: string } }) {
+  const { posts, totalPost } = await getAllPosts(page);
+
+  console.log(posts);
+
+  return (
+    <div className="flex flex-col gap-2">
+      <Category />
+      <div className="flex flex-col gap-2">
+        <div className="flex justify-between text-xs border-b-2 pb-2 border-dashed">
+          <span>전체 게시물</span>
+        </div>
+        <ul className="flex flex-col gap-2">
+          {posts.map((list: PostType) => (
+            <List key={list.title} {...list} />
+          ))}
+        </ul>
+        <ul>
+          <Link href={'/community?category=all&page=2'}>2</Link>
+        </ul>
+      </div>
+    </div>
+  );
 }
