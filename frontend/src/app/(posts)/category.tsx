6dfaf7c02c14b474fcf863cd5e7c681d8d91ea69
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-15 17:17:03 +0900

    feat: 카테고리 페이지

--- frontend/src/app/(posts)/category.tsx
+++ frontend/src/app/(posts)/category.tsx
@@ -1,3 +1,19 @@
+import { CATEGORY } from '@/constant/constant';
+import Link from 'next/link';
+
 export default function Category() {
-  return <div></div>;
+  const category = Object.keys(CATEGORY);
+
+  return (
+    <div className="w-full min-h-[150px] text-xs border border-lightBlue p-default">
+      <p className="border-b border-dashed border-darkBlue pb-default">카테고리</p>
+      <ul className="flex gap-2 pt-default">
+        {category.map(item => (
+          <li key={item}>
+            <Link href={`/posts?category=${CATEGORY[item]}`}>{item}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
