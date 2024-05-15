723836eae1a5f0425aa5e08b0ceae7c0a260aead
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-15 17:16:52 +0900

    feat: pagination link 수정

--- frontend/src/app/(posts)/pagination.tsx
+++ frontend/src/app/(posts)/pagination.tsx
@@ -4,20 +4,20 @@ import { PostType } from '@/type/community/type';
 import Link from 'next/link';
 
 interface PaginationProps {
-  data: PostType[];
   totalPosts: number;
   postsPerPage?: number;
 }
 
-export default function Pagination({ data, totalPosts, postsPerPage = 10 }: PaginationProps) {
+export default function Pagination({ totalPosts, postsPerPage = 10 }: PaginationProps) {
   const totalPages = Math.ceil(totalPosts / postsPerPage);
-  console.log(data);
 
   return (
     <ul className="flex justify-center">
       {Array.from({ length: totalPages }, (_, index) => (
         <li key={index} className="mr-2">
-          <Link href={`/posts?page=${index + 1}`} className="bg-blue-500 hover:bg-blue-700 font-bold py-1 px-2 rounded">
+          <Link
+            href={`/posts?page=${index + 1}`}
+            className="bg-blue-500 hover:bg-blue-700 font-bold py-1 px-default rounded">
             {index + 1}
           </Link>
         </li>
