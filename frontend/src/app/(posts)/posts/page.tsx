321aef9955df513d4faa1706b931ee8d199a2964
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-15 17:16:04 +0900

    feat: 통합검색 X 게시글 검색에서 search 있을 시 api

--- frontend/src/app/(posts)/posts/page.tsx
+++ frontend/src/app/(posts)/posts/page.tsx
@@ -2,30 +2,36 @@ import { api } from '@/util/api';
 import Category from '../category';
 import Lists from '../lists';
 import Pagination from '../pagination';
-import { GetServerSidePropsContext } from 'next';
 
-export async function getAllPosts(page: string) {
+interface URLProps {
+  params: {};
+  searchParams: { [key: string]: string };
+}
+
+export async function getAllPosts(page?: string, category?: string, search?: string) {
   try {
-    const res = await api.get(`/posts?category=all&page=${page}`);
+    // const API = search ? `/posts?category=${category}&page=${page}` : `/search?keyword=${search}`;
+    const API = `/posts?category=${category}&page=${page}`;
+    const res = await api.get(API);
     return res.data;
   } catch (err) {
     return console.log(`check ${err}`);
   }
 }
 
-export default async function Page(context: GetServerSidePropsContext) {
-  // const { posts, totalPosts } = await getAllPosts(page);
-  const { posts, totalPosts } = await getAllPosts(context.searchParams.page);
+export default async function Page(props: URLProps) {
+  const pageParam = props.searchParams.page;
+  const categoryParam = props.searchParams.category;
+  const { posts, totalPosts } = await getAllPosts(pageParam, categoryParam);
+  console.log(props, categoryParam, pageParam);
 
   return (
     <div className="flex flex-col gap-2">
       <Category />
       <div className="flex flex-col gap-2">
-        <div className="flex justify-between text-xs border-b-2 pb-2 border-dashed">
-          <span>전체 게시물</span>
-        </div>
-        <Lists posts={posts} />
-        <Pagination data={posts} totalPosts={totalPosts} />
+        <p className="text-xs border-b border-dashed border-darkBlue pb-default">전체 게시물</p>
+        {posts.length ? <Lists posts={posts} /> : <div className="flex justify-center">작성된 게시글이 없습니다.</div>}
+        {totalPosts !== 0 && <Pagination totalPosts={totalPosts} />}
       </div>
     </div>
   );
