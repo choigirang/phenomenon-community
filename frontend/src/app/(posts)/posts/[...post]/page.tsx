47311e3abd42bac9cf42b9bcc26c15385ee79b40
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-16 20:58:09 +0900

    feat: 개별 게시글 기본 구성 (+댓글, 좋아요 등)

--- frontend/src/app/(posts)/posts/[...post]/page.tsx
+++ frontend/src/app/(posts)/posts/[...post]/page.tsx
@@ -1,4 +1,9 @@
+import { PostType } from '@/type/community/type';
 import { api } from '@/util/api';
+import Content from './content';
+import Like from './like';
+import Comment from '@/app/(common)/(comment)/comment';
+import AddComment from '@/app/(common)/(comment)/addComment';
 
 async function getPostData(postNum: string) {
   try {
@@ -10,11 +15,42 @@ async function getPostData(postNum: string) {
 }
 
 export default async function Page({ params: { post } }: { params: { post: string[] } }) {
-  const data = await getPostData(post[0]);
+  const data: PostType = await getPostData(post[0]);
 
   return (
-    <main className="p-container">
-      <h2 className="text-lg text-blue border-b-4 border-darkBlue">게시글</h2>
-    </main>
+    <section className="flex flex-col gap-4 p-container">
+      <h2 className="text-lg text-blue border-b-4 border-darkBlue font-bold">게시글</h2>
+      {/* 작성자 정보 */}
+      <div className="flex flex-col border-b">
+        <h2 className="font-bold py-default">{data.title}</h2>
+        <div className="flex justify-between gap-3 text-xs py-default">
+          <div className="flex gap-3">
+            <h3>{data.author}</h3>
+            <span>|</span>
+            <h3>{data.date}</h3>
+          </div>
+          <div className="flex gap-3">
+            <span>조회 : {data.views}</span>
+            <span>스크랩 : {data.likes.length}</span>
+          </div>
+        </div>
+      </div>
+      {/* 글 내용 */}
+      <Content body={data.body} />
+      {/* 추천수 */}
+      <div className="flex justify-center">
+        <Like {...data} />
+      </div>
+      {/* 댓글 */}
+      <div className="flex gap-1 text-xs">
+        <h3 className="font-bold">전체 댓글</h3>
+        <span className="text-red">{data.comments.length}</span>
+      </div>
+      {data.comments.map(each => (
+        <Comment data={each} key={each.commentNumber} />
+      ))}
+      {/* 댓글 작성 */}
+      <AddComment />
+    </section>
   );
 }
