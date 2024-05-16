d8630d6f634f1f9c00003ec83b2794d3499ee7cf
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-16 21:00:01 +0900

    feat : reducer init data type

--- frontend/src/type/user/type.ts
+++ frontend/src/type/user/type.ts
@@ -1,12 +1,22 @@
 import { Likes } from '../common';
 
+export interface LoginIni {
+  img: string;
+  id: string;
+  name: string;
+  mail: string;
+  super: boolean;
+  login: boolean;
+  likes: Array<Likes>;
+}
+
 export interface UserType {
   img: string;
   id: string;
-  password: string;
+  password?: string;
   name: string;
   mail: string;
-  refreshToken: string;
+  refreshToken?: string;
   super: boolean;
   likes: Array<Likes>;
 }
