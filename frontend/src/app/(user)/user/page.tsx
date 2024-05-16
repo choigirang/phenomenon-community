20d9a1bc7cef23fcaffb5d8acbeb2e076e2c960b
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-16 20:58:45 +0900

    feat: user page with user card mapping

--- frontend/src/app/(user)/user/page.tsx
+++ frontend/src/app/(user)/user/page.tsx
@@ -1,3 +1,29 @@
-export default function Page() {
-  return <div></div>;
+import { UserType } from '@/type/user/type';
+import { api } from '@/util/api';
+import Card from './card';
+import Search from './search';
+
+async function getUser(user: string) {
+  try {
+    const res = await api.get(`/users`);
+    return res.data;
+  } catch (err) {
+    return console.log('check err:', err);
+  }
+}
+
+export default async function Page({ params: { id } }: { params: { id: string } }) {
+  const { findAllUser }: { findAllUser: UserType[] } = await getUser(id);
+
+  return (
+    <section className="flex flex-col">
+      <Search>
+        <ul className="grid grid-cols-5 gap-2">
+          {findAllUser.map(user => (
+            <Card key={user.id} {...user} />
+          ))}
+        </ul>
+      </Search>
+    </section>
+  );
 }
