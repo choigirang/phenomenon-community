3d0f6d7f6b6fb46eaf8f8ed6c49b1812b074e128
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-16 20:58:18 +0900

    feat: user page

---
+++ frontend/src/app/(user)/user/[...id]/page.tsx
@@ -0,0 +1,27 @@
+import { URL } from '@/constant/constant';
+import { UserType } from '@/type/user/type';
+import { api } from '@/util/api';
+import Image from 'next/image';
+
+async function getUser(user: string) {
+  try {
+    const res = await api.get(`/searchUser?id=${user}`);
+    return res.data;
+  } catch (err) {
+    return console.log('check err:', err);
+  }
+}
+
+export default async function Page({ params: { id } }: { params: { id: string } }) {
+  const { findUser } = await getUser(id[0]);
+  const user = findUser[0];
+
+  return (
+    <div className="grid grid-cols-user">
+      <div className="flex flex-col p-default border-2 border-lightBlue">
+        <Image src={`${URL + user.img}`} alt="user img" width={100} height={100} />
+        <span>{user.id}</span>
+      </div>
+    </div>
+  );
+}
