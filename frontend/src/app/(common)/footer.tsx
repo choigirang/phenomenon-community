b0d0f198a46cc4dc77581bd73763d98f76768f5d
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-16 20:57:14 +0900

    re-folder

--- frontend/src/app/(common)/footer.tsx
+++
@@ -1,3 +0,0 @@
-export default function Footer() {
-  return <footer className="w-full h-[300px] bg-blue"></footer>;
-}
--- frontend/src/app/(home)/existLogin.tsx
+++
@@ -1,3 +0,0 @@
-export default function ExistLogin() {
-  return <div></div>;
-}
