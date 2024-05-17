7ff4f5bb2ab1cca7325b5469bbe6877147993630
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-17 17:52:56 +0900

    feat: redux => localstorage user, post의 like 와 유저 매칭

--- frontend/src/app/(posts)/posts/[...post]/like.tsx
+++ frontend/src/app/(posts)/posts/[...post]/like.tsx
@@ -9,38 +9,39 @@ import { api } from '@/util/api';
 import { PostType } from '@/type/community/type';
 
 export default function Like(data: PostType) {
-  {
-    /** ssr 좋아요 반영을 위한 초깃값 설정 */
-  }
+  const [userId, setUserId] = useState('');
+  // ssr 좋아요 반영을 위한 초깃값 설정
   const [like, setLike] = useState({ click: false, length: data.likes.length });
 
-  {
-    /** 로그인한 유저의 좋아요 확인*/
-  }
-  const loginUser: LoginIni = useAppSelector(state => state.loginSlice);
-  const checkLikeWithUser = data.likes.find(user => user === loginUser.id);
-
   const handleLike = () => {
-    if (!loginUser.id) return alert('로그인이 필요합니다.');
-    else {
-      api.post(`/post/likes`, { id: loginUser.id, postNumber: data.postNumber });
-      setLike(prev => ({ ...prev, click: !prev }));
-    }
+    if (!userId) return alert('로그인이 필요합니다.');
+
+    api.post(`/post/likes`, { id: userId, postNumber: data.postNumber });
+    setLike(prev => {
+      // 좋아요 상태에 따라 좋아요 수 조정
+      const newLength = prev.click ? prev.length - 1 : prev.length + 1;
+      return { length: newLength, click: !prev.click };
+    });
   };
 
   useEffect(() => {
+    //  로그인한 유저 확인
+    const savedData = window.localStorage.getItem('user');
+    const loginUser: LoginIni = savedData !== null && JSON.parse(savedData);
+    if (loginUser.id) setUserId(prev => loginUser.id);
+
+    // 좋아요 확인
+    const checkLikeWithUser = data.likes.find(user => user === loginUser.id);
     if (checkLikeWithUser) setLike(prev => ({ ...prev, click: true }));
-  }, [loginUser]);
+  }, []);
 
   return (
     <div className="flex flex-col">
       <div className="flex justify-center items-center gap-2 border border-b-0 p-default">
-        <span>{data.likes.length}</span>
-        {like.click ? (
-          <On width={32} height={32} color="red" onClick={handleLike} />
-        ) : (
-          <Off width={32} height={32} onClick={handleLike} />
-        )}
+        <span>{like.length}</span>
+        <button type="button" onClick={handleLike}>
+          {like.click ? <On width={32} height={32} color="red" /> : <Off width={32} height={32} />}
+        </button>
       </div>
       <div className="flex border">
         <span className="p-default">공유하기</span>
