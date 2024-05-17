9ec0ad1844cca77782f5063b3649743a12b53600
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-17 17:54:28 +0900

    feat: redux => localstorage (handle login in hooks)

--- frontend/src/hooks/useLogin.tsx
+++ frontend/src/hooks/useLogin.tsx
@@ -1,39 +1,51 @@
 'use client';
 
-import { FormEvent, useState } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 import { api } from '../util/api';
 import useInputs from './useInputs';
-import { LoginIni, UserType } from '@/type/user/type';
-import { useAppDispatch } from './useRedux';
-import { login, logout } from '@/store/modules/loginSlice';
+import { InitLoginData, UserType } from '@/type/user/type';
+
+const initLogin: InitLoginData = {
+  img: '',
+  id: '',
+  name: '',
+  super: false,
+};
 
 export default function useLogin() {
-  const [userLogin, setUserLogin] = useState({ save: false, auto: false });
+  const [userLogin, setUserLogin] = useState({ img: '', id: '', name: '', super: false });
   const [id, setId] = useInputs('');
   const [pass, setPass] = useInputs('');
-
-  const dispatch = useAppDispatch();
 
   const handleLogin = (e: FormEvent) => {
     e.preventDefault();
     api
       .post('/login', { id, password: pass })
       .then(res => {
-        const { id, name, mail, img, super: isSuper, likes }: UserType = res.data.user;
-        const userData = { id, name, mail, img, super: isSuper, likes };
-        return userData;
-      })
-      .then(res => {
-        dispatch(login(res));
+        const { id, name, img, super: isSuper }: UserType = res.data.user;
+        const userData = { id, name, img, super: isSuper };
+
+        // 로컬 저장
+        window.localStorage.setItem(
+          'user',
+          JSON.stringify({ img: userData.img, id: userData.id, name: userData.name, super: userData.super }),
+        );
+        setUserLogin({ ...userData });
         alert('로그인 되었습니다.');
+        return userData;
       })
       .catch(res => alert('아이디와 비밀번호를 확인하세요.'));
   };
 
   const handleLogout = () => {
-    dispatch(logout());
+    window.localStorage.removeItem('user');
+    setUserLogin({ ...initLogin });
     alert('로그아웃 되었습니다.');
   };
 
-  return { id, pass, setUserLogin, setId, setPass, handleLogin, handleLogout };
+  useEffect(() => {
+    console.log('userLoginUpdate');
+  }, [userLogin]);
+
+  return { userLogin, id, pass, setUserLogin, setId, setPass, handleLogin, handleLogout };
 }
