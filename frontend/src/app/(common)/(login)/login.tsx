060616e877e9ee99235bf64696d3ea2a1eb6e9ad
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-17 17:51:27 +0900

    feat: redux => localstorage login user

--- frontend/src/app/(common)/(login)/login.tsx
+++ frontend/src/app/(common)/(login)/login.tsx
@@ -2,23 +2,33 @@
 
 import Link from 'next/link';
 import NotLogin from './notLogin';
-import { useAppSelector } from '@/hooks/useRedux';
-import { LoginIni } from '@/type/user/type';
 import ExistLogin from './existLogin';
+import useLogin from '@/hooks/useLogin';
+import { useEffect } from 'react';
+import useStorage from '@/hooks/useStorage';
 
 const STYLE = {
   container: 'flex justify-center items-center w-full min-h-[120px] p-3 text-xs border border-blue self-start',
 };
 
 export default function Login() {
-  const user: LoginIni = useAppSelector(state => state.loginSlice);
+  // 로그인 훅
+  const { userLogin, setUserLogin, id, pass, setId, setPass, handleLogin, handleLogout } = useLogin();
+  // 로컬 훅
+  const { storageId, parseUser } = useStorage();
+
+  const needNotLogin = { id, pass, setId, setPass, handleLogin };
+
+  useEffect(() => {
+    if (storageId) setUserLogin(prev => ({ ...parseUser }));
+  }, []);
 
   return (
     <div className={STYLE.container}>
-      {user.id ? (
-        <ExistLogin {...user} />
+      {userLogin.id ? (
+        <ExistLogin userLogin={userLogin} handleLogout={handleLogout} />
       ) : (
-        <NotLogin>
+        <NotLogin props={needNotLogin}>
           <Link href={'/user/sign'}>회원가입</Link>
           <Link href={'/user/findId'}>아이디·비밀번호</Link>
         </NotLogin>
--- frontend/src/app/(common)/(login)/notLogin.tsx
+++ frontend/src/app/(common)/(login)/notLogin.tsx
@@ -1,8 +1,11 @@
 'use client';
-
-import useLogin from '@/hooks/useLogin';
+import { LoginProps } from '@/type/user/type';
 import React from 'react';
-import { useDispatch } from 'react-redux';
+
+interface Props {
+  children: React.ReactNode;
+  props: Partial<LoginProps>;
+}
 
 const STYLE = {
   form: 'grid grid-cols-login w-full gap-3',
@@ -12,15 +15,26 @@ const STYLE = {
   button: 'flex justify-center items-center p-2 text-white bg-lightBlue',
 };
 
-export default function NotLogin({ children }: React.PropsWithChildren) {
-  const { id, pass, setId, setPass, setUserLogin, handleLogin } = useLogin();
-
+export default function NotLogin({ children, props }: Props) {
   return (
-    <form className={STYLE.form} onSubmit={handleLogin}>
+    <form className={STYLE.form} onSubmit={props.handleLogin} name="login">
       {/* Id & Pass */}
       <div className={STYLE.flexCol}>
-        <input type="text" placeholder="테스트 아이디 : test" value={id} onChange={setId} className={STYLE.input} />
-        <input type="password" placeholder="비밀번호 : 1234" value={pass} onChange={setPass} className={STYLE.input} />
+        <input
+          type="text"
+          placeholder="테스트 아이디 : test"
+          value={props.id}
+          onChange={props.setId}
+          className={STYLE.input}
+        />
+        <input
+          type="password"
+          placeholder="비밀번호 : 1234"
+          value={props.pass}
+          onChange={props.setPass}
+          className={STYLE.input}
+          autoComplete="current-password"
+        />
 
         {/* Sign & find info children */}
         <div className={`${STYLE.flexRows} justify-between`}>{children}</div>
@@ -29,14 +43,14 @@ export default function NotLogin({ children }: React.PropsWithChildren) {
       {/* Login Options */}
       <div className={`${STYLE.flexCol} text-xs`}>
         <div className={STYLE.flexRows}>
-          <input type="checkbox" onChange={() => setUserLogin(prev => ({ ...prev, save: !prev.save }))} />
+          {/* <input type="checkbox" onChange={() => setUserLogin(prev => ({ ...prev, save: !prev.save }))} /> */}
           <span>아이디 저장</span>
         </div>
         <div className={STYLE.flexRows}>
-          <input type="checkbox" onChange={() => setUserLogin(prev => ({ ...prev, auto: !prev.auto }))} />
+          {/* <input type="checkbox" onChange={() => setUserLogin(prev => ({ ...prev, auto: !prev.auto }))} /> */}
           <span>자동 로그인</span>
         </div>
-        <button type="submit" className={STYLE.button} onClick={handleLogin}>
+        <button type="submit" className={STYLE.button} onClick={props.handleLogin}>
           로그인
         </button>
       </div>
