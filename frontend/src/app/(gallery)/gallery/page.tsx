33b2db2d993bc1d85c9f9a87e8c4ffdf712283eb
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-20 00:49:59 +0900

    feat: gallery all page (lists, pagination)

--- frontend/src/app/(gallery)/gallery/page.tsx
+++ frontend/src/app/(gallery)/gallery/page.tsx
@@ -1,3 +1,34 @@
-export default function Page() {
-  return <div></div>;
+import { GalleryType } from '@/type/gallery/type';
+import { api } from '@/util/api';
+import List from '../list';
+import Pagination from '@/app/(common)/pagination';
+import { SearchParams } from '@/type/common';
+
+export async function getAllPosts(page?: string, category: string = 'all', search?: string) {
+  try {
+    // const API = search ? `/posts?category=${category}&page=${page}` : `/search?keyword=${search}`;
+    const API = `/gallery?page=${page}`;
+    const res = await api.get(API);
+    return res.data;
+  } catch (err) {
+    return console.log(`check ${err}`);
+  }
+}
+
+export default async function Page(props: SearchParams) {
+  const pageParam = props.searchParams.page;
+
+  const { gallery, totalGallery } = await getAllPosts(pageParam);
+  return (
+    <section>
+      <ul className="grid grid-cols-preGallery aspect-auto gap-3">
+        {gallery.length ? (
+          gallery.map((data: GalleryType) => <List key={data.galleryNumber} {...data} />)
+        ) : (
+          <div className="flex justify-center">작성된 게시글이 없습니다.</div>
+        )}
+      </ul>
+      <Pagination src={'gallery'} total={totalGallery} />
+    </section>
+  );
 }
