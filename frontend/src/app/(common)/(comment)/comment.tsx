d1ddc340e247c0472ace3c5933946b9fbfe16b43
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-19 23:32:08 +0900

    feat: page mapping comment 에서 comment use client
    page에서 comment data props 를 이용하여
    comment data setting

--- frontend/src/app/(common)/(comment)/comment.tsx
+++ frontend/src/app/(common)/(comment)/comment.tsx
@@ -1,17 +1,34 @@
+'use client';
+
 import { CommentData } from '@/type/common';
+import React, { useState } from 'react';
+import AddComment from './addComment';
+import { PostType } from '@/type/community/type';
 
 interface CommentProps {
-  data: CommentData;
+  data: PostType;
+  comment: CommentData[];
 }
 
-export default function Comment({ data }: CommentProps) {
+export default function Comment({ data, comment }: CommentProps) {
+  const [comments, setComments] = useState<CommentData[] | []>(comment);
+
   return (
-    <div className="grid grid-cols-comment text-xs pt-default border-t border-gray">
-      <div className="flex gap-4">
-        <h2 className="font-semibold">{data.author}</h2>
-        <span>{data.comment}</span>
-      </div>
-      <span className="text-right text-gray">{data.date}</span>
-    </div>
+    <React.Fragment>
+      {/* 댓글 목록 */}
+      {comments.length === 0 && <div>작성된 댓글이 없습니다.</div>}
+
+      {comments.map(each => (
+        <ul className="grid grid-cols-comment text-xs pt-default border-t border-gray" key={each.commentNumber}>
+          <li className="flex gap-4">
+            <h2 className="font-semibold">{each.author}</h2>
+            <span>{each.comment}</span>
+          </li>
+          <li className="text-right text-gray">{each.date}</li>
+        </ul>
+      ))}
+      {/* 댓글 추가 */}
+      <AddComment setComments={setComments} data={data} />
+    </React.Fragment>
   );
 }
