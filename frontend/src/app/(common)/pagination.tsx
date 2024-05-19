554da6586ff49a833bd00e3b8760cae825b9f3a2
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-20 00:49:16 +0900

    feat: post & gallery 공통 컴포넌트

--- frontend/src/app/(common)/pagination.tsx
+++ frontend/src/app/(common)/pagination.tsx
@@ -4,19 +4,20 @@ import { PostType } from '@/type/community/type';
 import Link from 'next/link';
 
 interface PaginationProps {
-  totalPosts: number;
-  postsPerPage?: number;
+  src: string;
+  total: number;
+  perPage?: number;
 }
 
-export default function Pagination({ totalPosts, postsPerPage = 10 }: PaginationProps) {
-  const totalPages = Math.ceil(totalPosts / postsPerPage);
+export default function Pagination({ src, total, perPage = 10 }: PaginationProps) {
+  const totalPages = Math.ceil(total / perPage);
 
   return (
     <ul className="flex justify-center">
       {Array.from({ length: totalPages }, (_, index) => (
         <li key={index} className="mr-2">
           <Link
-            href={`/posts?page=${index + 1}`}
+            href={`/${src}?page=${index + 1}`}
             className="bg-blue-500 hover:bg-blue-700 font-bold py-1 px-default rounded">
             {index + 1}
           </Link>
