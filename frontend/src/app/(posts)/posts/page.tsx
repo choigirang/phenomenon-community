4ccdfb555db41d54919d7f61f5892c5f34762b8f
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-22 00:14:38 +0900

    feat: components => tag

--- frontend/src/app/(posts)/posts/page.tsx
+++ frontend/src/app/(posts)/posts/page.tsx
@@ -1,8 +1,9 @@
 import { api } from '@/util/api';
 import Category from '../category';
-import Lists from '../lists';
 import { SearchParams } from '@/type/common';
 import Pagination from '@/app/(common)/pagination';
+import { PostType } from '@/type/community/type';
+import List from '../list';
 
 export async function getAllPosts(page?: string, category: string = 'all', search?: string) {
   try {
@@ -25,7 +26,15 @@ export default async function Page(props: SearchParams) {
       <Category />
       <div className="flex flex-col gap-2">
         <p className="text-xs border-b border-dashed border-darkBlue pb-default">전체 게시물</p>
-        {posts.length ? <Lists posts={posts} /> : <div className="flex justify-center">작성된 게시글이 없습니다.</div>}
+        {posts.length ? (
+          <ul className="flex flex-col gap-2">
+            {posts.map((list: PostType) => (
+              <List key={list.title} {...list} />
+            ))}
+          </ul>
+        ) : (
+          <div className="flex justify-center">작성된 게시글이 없습니다.</div>
+        )}
         {totalPosts !== 0 && <Pagination src={'posts'} total={totalPosts} />}
       </div>
     </div>
