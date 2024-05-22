b44451aeb9782cdbc2d1df9d3a9b00c9a5795990
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-22 20:55:05 +0900

    style

--- frontend/src/app/(notice)/notice/page.tsx
+++ frontend/src/app/(notice)/notice/page.tsx
@@ -2,6 +2,7 @@ import Pagination from '@/app/(common)/pagination';
 import { Notice, SearchParams } from '@/type/common';
 import { api } from '@/util/api';
 import Link from 'next/link';
+import AddNotice from './addNotice';
 
 async function getData(page: string = '1') {
   const API = `/notice?page=${page}`;
@@ -16,14 +17,17 @@ export default async function Page(props: SearchParams) {
 
   return (
     <div className="flex flex-col">
-      <h2 className="w-full pb-default text-center text-blue text-xl font-bold">공지사항</h2>
-      <ul className="w-full flex flex-gap gap-2 border-y-4 border-blue">
+      <div className="flex justify-center items-center">
+        <h2 className="w-full pb-default text-center text-blue text-xl font-bold">공지사항</h2>
+        <AddNotice />
+      </div>
+      <ul className="w-full flex flex-col gap-2 text-sm border-y-4 border-blue">
         {notice.map((each: Notice) => (
           <li key={each.noticeNumber} className="w-full p-default border-b last:border-0">
             <Link href={`/notice/${each.noticeNumber}`} className="w-full grid grid-cols-notice">
               <span className="text-center text-blue">{each.noticeNumber}</span>
               <span>{each.title}</span>
-              <span className="text-center text-lightGray">{each.date}</span>
+              <span className="text-right text-lightGray">{each.date}</span>
             </Link>
           </li>
         ))}
