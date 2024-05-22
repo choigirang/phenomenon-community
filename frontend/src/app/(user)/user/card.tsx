721c0b338aac5487d0abf69eda6c407bcb63daa6
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-22 20:56:39 +0900

    feat: name => id property

--- frontend/src/app/(user)/user/card.tsx
+++ frontend/src/app/(user)/user/card.tsx
@@ -8,7 +8,7 @@ export default function Card(data: UserType) {
     <li className="flex justify-center items-center border border-gray/50 p-default">
       <Link href={`/user/${data.id}`} className="flex w-full h-full flex-col justify-center items-center gap-2">
         <Image src={PROFILE_URL(data.img)} alt="user img" width={70} height={70} className="rounded-lg" />
-        <h3>{data.name}</h3>
+        <h3>{data.id}</h3>
       </Link>
     </li>
   );
