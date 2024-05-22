29cafba2b82801c8b10dc24f0e03e455e9664c31
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-22 20:57:19 +0900

    feat: user page 검색 기능

--- frontend/src/app/(user)/user/page.tsx
+++ frontend/src/app/(user)/user/page.tsx
@@ -1,28 +1,41 @@
 import { UserType } from '@/type/user/type';
 import { api } from '@/util/api';
-import Card from './card';
 import Search from './search';
+import { SearchParams } from '@/type/common';
+import Card from './card';
 
 async function getUser(user: string) {
   try {
-    const res = await api.get(`/users`);
-    return res.data;
+    if (user) {
+      const res = await api.get(`/searchUser?id=${user}`);
+      return res.data;
+    } else {
+      const res = await api.get('/users');
+      return res.data;
+    }
   } catch (err) {
     return console.log('check err:', err);
   }
 }
 
-export default async function Page({ params: { id } }: { params: { id: string } }) {
-  const { findAllUser }: { findAllUser: UserType[] } = await getUser(id);
+export default async function Page(params: SearchParams) {
+  const user = params.searchParams.search;
+  const data = await getUser(user);
+
+  const userData: UserType[] = user ? data.findUser : data.findAllUser;
 
   return (
-    <section className="flex flex-col">
+    <section className="flex flex-col gap-2">
       <Search>
-        <ul className="grid grid-cols-5 gap-2">
-          {findAllUser.map(user => (
-            <Card key={user.id} {...user} />
-          ))}
-        </ul>
+        {userData ? (
+          <ul className="grid grid-cols-5 gap-2">
+            {userData.map(user => (
+              <Card key={user.id} {...user} />
+            ))}
+          </ul>
+        ) : (
+          <div className="w-full text-center">검색된 유저가 없습니다.</div>
+        )}
       </Search>
     </section>
   );
