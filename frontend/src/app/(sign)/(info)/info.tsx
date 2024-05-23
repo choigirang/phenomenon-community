6cfa43ff78a2defae862effc368029c4d02e3fd7
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-23 19:39:43 +0900

    feat: sign info page

--- frontend/src/app/(sign)/(info)/info.tsx
+++ frontend/src/app/(sign)/(info)/info.tsx
@@ -1,10 +1,14 @@
 'use client';
 
 import useSignInfo from '@/hooks/useSignInfo';
-import { AxiosSecurityCode, CheckId, CheckName, CheckPass, CheckSecurityType, InputType } from '@/type/sign/type';
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 import Id from './id';
+import Profile from './profile';
+import Password from './password';
+import Name from './name';
+import Email from './email';
+import Security from './security';
 
 export default function Info() {
   const {
@@ -35,8 +39,28 @@ export default function Info() {
         onSubmit={agreementCheck}
         method="post"
         encType="multipart/form-data"
-        className="flex flex-col px-[10%] py-[5%]">
+        className="flex flex-col px-[10%] py-[5%] border border-lightGray">
+        <Profile selectedImage={selectedImage} setSelectedImage={setSelectedImage} />
+
         <Id id={id} setId={setId} />
+
+        <Name nickName={name} setNickName={setName} />
+
+        <Password checkPass={pass} setCheckPass={setPass} />
+
+        <Email userMail={mail} setUserMail={setMail} inputAble={inputAble} setInputAble={setInputAble}>
+          <Security
+            security={security}
+            setSecurity={setSecurity}
+            checkSecurity={checkSecurity}
+            checkMailOpt={checkMailOpt}
+            setCheckSecurity={setCheckSecurity}
+            userMail={mail}
+          />
+        </Email>
+        <button type="button" onClick={agreementCheck} className="p-default bg-blue text-white">
+          다음
+        </button>
       </form>
     </React.Fragment>
   );
