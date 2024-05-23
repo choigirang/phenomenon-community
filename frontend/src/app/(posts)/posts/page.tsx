2eb7507439b8116727d12360131bc84887dcc017
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-23 19:39:07 +0900

    feat: search 추가에 따른 post keyword

--- frontend/src/app/(posts)/posts/page.tsx
+++ frontend/src/app/(posts)/posts/page.tsx
@@ -4,11 +4,11 @@ import { SearchParams } from '@/type/common';
 import Pagination from '@/app/(common)/pagination';
 import { PostType } from '@/type/community/type';
 import List from '../list';
+import Search from '@/app/(common)/(aboutContent)/(content)/search';
 
-export async function getAllPosts(page?: string, category: string = 'all', search?: string) {
+export async function getAllPosts(page: string = '1', category: string = 'all', search?: string) {
   try {
-    // const API = search ? `/posts?category=${category}&page=${page}` : `/search?keyword=${search}`;
-    const API = `/posts?category=${category}&page=${page}`;
+    const API = search ? `/posts/search?page=${page}&keyword=${search}` : `/posts?category=${category}&page=${page}`;
     const res = await api.get(API);
     return res.data;
   } catch (err) {
@@ -19,7 +19,8 @@ export async function getAllPosts(page?: string, category: string = 'all', searc
 export default async function Page(props: SearchParams) {
   const pageParam = props.searchParams.page;
   const categoryParam = props.searchParams.category;
-  const { posts, totalPosts } = await getAllPosts(pageParam, categoryParam);
+  const keyword = props.searchParams.keyword;
+  const { posts, totalPosts } = await getAllPosts(pageParam, categoryParam, keyword);
 
   return (
     <div className="flex flex-col gap-2">
@@ -36,6 +37,7 @@ export default async function Page(props: SearchParams) {
           <div className="flex justify-center">작성된 게시글이 없습니다.</div>
         )}
         {totalPosts !== 0 && <Pagination src={'posts'} total={totalPosts} />}
+        <Search src="posts" placeholder="검색할 게시글의 제목을 입력하세요." />
       </div>
     </div>
   );
