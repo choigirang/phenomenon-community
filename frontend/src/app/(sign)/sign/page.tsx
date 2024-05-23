319fd3d784b8092499b1654fc8a0e1951d44dd02
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-23 19:40:26 +0900

    feat: path 에 따른 process

--- frontend/src/app/(sign)/sign/page.tsx
+++ frontend/src/app/(sign)/sign/page.tsx
@@ -2,15 +2,26 @@ import { SearchParams } from '@/type/common';
 import Process from '../process';
 import Agree from '../agree';
 import Info from '../(info)/info';
+import Complete from '../complete';
+import Link from 'next/link';
 
 export default function Page(props: SearchParams) {
   const path = props.searchParams.page;
 
   return (
     <div className="flex flex-col gap-4 w-full">
-      <Process path={path} />
+      {path && <Process path={path} />}
       {path === 'agree' && <Agree />}
       {path === 'info' && <Info />}
+      {path === 'complete' && <Complete />}
+      {!path && (
+        <div className="flex flex-col gap-2 justify-center items-center w-full h-[300px]">
+          잘못된 접근입니다.
+          <Link href={'/'} className="p-default bg-blue text-white">
+            홈으로 이동
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
