2a760cf5aa87d33b183c4e17a8b00b01f415bf97
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-23 19:40:59 +0900

    feat: add with login layout dynamic

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,22 +3,28 @@ import Header from './(common)/(header)/header';
 import Footer from './(common)/(footer)/footer';
 import Login from './(common)/(login)/login';
 import WithProvider from '@/util/provider/withProvider';
-import WithLogin from './(common)/(login)/withLogin';
+import dynamic from 'next/dynamic';
 
 interface RootLayoutProps {
   children: React.ReactNode;
   showLogin?: boolean;
 }
 
+const WithLogin = dynamic(() => import('./(common)/(login)/withLogin'), {
+  ssr: false,
+});
+
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="kr">
-      <body>
-        <WithProvider>
-          <Header />
-          <WithLogin>{children}</WithLogin>
-          <Footer />
-        </WithProvider>
+      <body className="flex flex-col justify-between">
+        <div>
+          <WithProvider>
+            <Header />
+            <WithLogin>{children}</WithLogin>
+          </WithProvider>
+        </div>
+        <Footer />
       </body>
     </html>
   );
