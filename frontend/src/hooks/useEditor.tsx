5603a85fd4c0171a0a82f40b0bac069e02773c10
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-25 20:13:10 +0900

    *err: useEditor dateHandler 주석 처리
    draft css of useEditor 404err
    => script로 실행됨
    => useEditor를 사용하는 모든 컴포넌트가 page 자체를 스크립트로 실행되게 만듬
    
    => dataHandler 따로 만들거나 || css error 해결 후 재시도 필요

--- frontend/src/hooks/useEditor.tsx
+++ frontend/src/hooks/useEditor.tsx
@@ -54,16 +54,5 @@ export default function useEditor(htmlStr: string) {
     image: { uploadCallback: uploadCallback },
   };
 
-  // create date func
-  const dateHandler = () => {
-    const currentDate = new Date();
-    const year = currentDate.getFullYear();
-    const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작하므로 +1 필요
-    const day = String(currentDate.getDate()).padStart(2, '0');
-
-    const formattedDate = `${year}-${month}-${day}`;
-    return formattedDate;
-  };
-
-  return { content, editorState, toolbar, dateHandler, onEditorStateChange };
+  return { content, editorState, toolbar, onEditorStateChange };
 }
