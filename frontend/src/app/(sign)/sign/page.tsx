1b80d8efe7a3d6890171e0d4a025f91311cfebfe
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-25 20:11:50 +0900

    feat: add dynamic meta / Signpage

--- frontend/src/app/(sign)/sign/page.tsx
+++ frontend/src/app/(sign)/sign/page.tsx
@@ -5,6 +5,21 @@ import Info from '../info';
 import Complete from '../complete';
 
 import { SearchParams } from '@/type/common';
+import { getMetadata } from '@/constant/metaData';
+import { Metadata } from 'next';
+
+/** sign page meta */
+export const generateMetadata = async (props: SearchParams): Promise<Metadata> => {
+  const path = props.searchParams.page;
+
+  const pathTitle: { [key: string]: string } = {
+    agree: '필수 동의',
+    info: '개인 정보 입력',
+    complete: '회원 가입 완료',
+  };
+
+  return getMetadata({ title: pathTitle[path], asPath: `/sign?page=${path}` });
+};
 
 /** 2024/05/23 - second process page in Sign page */
 export default function Page(props: SearchParams) {
