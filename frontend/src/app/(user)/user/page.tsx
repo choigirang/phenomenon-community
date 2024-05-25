580399ad7b526f3a42fe5ba7ff203f0ea2bd358c
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-25 20:12:05 +0900

    feat: add dynamic meta / All Userpage

--- frontend/src/app/(user)/user/page.tsx
+++ frontend/src/app/(user)/user/page.tsx
@@ -1,18 +1,34 @@
+import { Metadata } from 'next';
 import Search from '../../(common)/(aboutContent)/(content)/search';
 import Card from '../card';
+import Pagination from '@/app/(common)/pagination';
 import { api } from '@/util/api';
+import { getMetadata } from '@/constant/metaData';
 
 import { SearchParams } from '@/type/common';
 import { UserType } from '@/type/user/type';
 
+/** user page meta */
+export const generateMetadata = async (params: SearchParams): Promise<Metadata> => {
+  const pageParam = params.searchParams.page || 1;
+  const user = params.searchParams.keyword;
+
+  function title() {
+    if (user) return `${user} 검색 ${pageParam} 페이지`;
+    else return `유저 검색 ${pageParam} 페이지`;
+  }
+
+  return getMetadata({ title: title(), asPath: '/user' });
+};
+
 // (user id && user) || all users
-async function getUser(user: string) {
+async function getUser(page: string = '1', user: string) {
   try {
     if (user) {
-      const res = await api.get(`/searchUser?id=${user}`);
+      const res = await api.get(`/users?id=${user}`);
       return res.data;
     } else {
-      const res = await api.get('/users');
+      const res = await api.get(`/users?page=${page}`);
       return res.data;
     }
   } catch (err) {
@@ -22,18 +38,17 @@ async function getUser(user: string) {
 
 /** 2024/05/23 - user list page */
 export default async function Page(params: SearchParams) {
+  const pageParam = params.searchParams.page;
   const user = params.searchParams.keyword;
-  const data = await getUser(user);
-
   // data of search user
-  const userData: UserType[] = user ? data.findUser : data.findAllUser;
+  const { users, totalUsers } = await getUser(pageParam, user);
 
   return (
     <section className="flex flex-col gap-2">
       <Search src="user" placeholder="아이디를 입력해주세요.">
-        {userData ? (
+        {users ? (
           <ul className="grid grid-cols-5 gap-2">
-            {userData.map(user => (
+            {users.map((user: UserType) => (
               <Card key={user.id} {...user} />
             ))}
           </ul>
@@ -41,6 +56,7 @@ export default async function Page(params: SearchParams) {
           <div className="w-full text-center">검색된 유저가 없습니다.</div>
         )}
       </Search>
+      {totalUsers !== 0 && <Pagination src={'user'} total={totalUsers} />}
     </section>
   );
 }
