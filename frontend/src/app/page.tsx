f3395c4b99e6860ab3380baf0953799fdc2e8682
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-27 17:17:48 +0900

    chore: add meta

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,15 @@
+import { Metadata } from 'next';
 import React from 'react';
 import PrePosts from './(posts)/prePosts';
 import PreGallery from './(gallery)/preGallery';
+import { getMetadata } from '@/constant/metaData';
+
+/** home page meta */
+export const generateMetadata = async (): Promise<Metadata> => {
+  return getMetadata({
+    asPath: `/`,
+  });
+};
 
 /** 2024/05/11 - home page with latest gallery & latest post */
 export default function Page() {
